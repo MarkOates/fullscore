673018a118f504c039dbdf313f2a5798e1d05705
Mark Oates <euph_boy@hotmail.com>
2015-04-03 00:26:53 -0400

    moved PlaybackControl to .hpp/.cpp files

---
+++ include/fullscore/playback_control.h
@@ -0,0 +1,33 @@
+#ifndef __FULLSCORE_PLAYBACK_CONTROL
+#define __FULLSCORE_PLAYBACK_CONTROL
+
+
+
+
+
+#include <fullscore/playback_device_interface.h>
+#include <fullscore/measure_grid.h>
+
+
+class PlaybackControl
+{
+public:
+	float position; // in seconds
+	bool playing;
+	float tempo_duration;
+	double tempo_bpm;
+	MeasureGrid *measure_grid;
+	PlaybackDeviceInterface *playback_device;
+
+	PlaybackControl(MeasureGrid *measure_grid, PlaybackDeviceInterface *playback_device);
+
+	void toggle_playback();
+	void reset();
+	void refresh_note_start_and_end_times();
+	void update(double time_now);
+};
+
+
+
+
+#endif
