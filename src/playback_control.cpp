51786602edb6bce7d5b11c92e4ecf60b5ba69316
Mark Oates <euph_boy@hotmail.com>
2015-04-03 22:21:25 -0400

    created a simple PitchTransform class for diatonic playback

--- src/playback_control.cpp
+++ src/playback_control.cpp
@@ -9,6 +9,43 @@
 
 
 
+class PitchTransform
+{
+public:
+	static int diatonic_in_c(int scale_degree, int accidental)
+		// given a diatonic scale degree (zero-based index, so scale
+		// degree ^1 is pitch==0, scale degree ^4 is pitch==3 and so on)
+		// returns the current MIDI pitch where pitch==0 is MIDDLE_C
+	{
+		int octave = 0;
+		while (scale_degree < 0) { scale_degree += 7; octave--; }
+		while (scale_degree >= 7) { scale_degree -= 7; octave++; }
+		int normalized_scale_degree = scale_degree % 7;
+
+		switch(normalized_scale_degree)
+		{
+		case 0: normalized_scale_degree = 0; break;
+		case 1: normalized_scale_degree = 2; break;
+		case 2: normalized_scale_degree = 4; break;
+		case 3: normalized_scale_degree = 5; break;
+		case 4: normalized_scale_degree = 7; break;
+		case 5: normalized_scale_degree = 9; break;
+		case 6: normalized_scale_degree = 11; break;
+		default: break;
+		}
+
+		return normalized_scale_degree + octave * 12 + accidental;
+	}
+	static int diatonic_in_c_alto_clef(int scale_degree, int accidental)
+	{
+		// this translates it so that what is on the staff is in treble_clef
+		return diatonic_in_c(scale_degree, accidental);
+	}
+};
+
+
+
+
 
 PlaybackControl::PlaybackControl(MeasureGrid *measure_grid, PlaybackDeviceInterface *playback_device)
 	: position(0)
@@ -69,14 +106,14 @@ void PlaybackControl::update(double time_now)
 				{
 					// attack the note
 					note.attacked = true;
-					if (playback_device) playback_device->note_on(y, note.scale_degree + 60, 127);
+					if (playback_device) playback_device->note_on(y, PitchTransform::diatonic_in_c_alto_clef(note.scale_degree, 0), 127);
 				}
 
 				if (note.attacked && position >= note.end_time)
 				{
 					// release the note
 					note.released = true;
-					if (playback_device) playback_device->note_off(y, note.scale_degree + 60);
+					if (playback_device) playback_device->note_off(y, PitchTransform::diatonic_in_c_alto_clef(note.scale_degree, 0));
 				}
 			}
 		}
