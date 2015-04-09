82cb962f2599504ac8dd057abcbc24a144dbbe66
Mark Oates <euph_boy@hotmail.com>
2015-04-09 14:44:44 -0400

    added basic transformation functions in the Measure class

--- src/measure.cpp
+++ src/measure.cpp
@@ -2,6 +2,8 @@
 // empty
 
 #include <fullscore/measure.h>
+#include <fullscore/note.h>
+
 
 int Measure::get_note_position(Note *note)
 {
@@ -16,3 +18,54 @@ Note *Measure::get_note_at(int index)
 	if (index < 0 || notes.empty() || index >= notes.size()) return NULL;
 	return notes[index];
 }
+
+
+void Measure::retrograde()
+{
+	std::vector<Note *> result;
+	for (unsigned i=notes.size()-1; i>=0; i--)
+		result.push_back(notes[i]);
+	notes = result;
+}
+
+
+void Measure::append(const Measure &other_measure)
+{
+	for (unsigned i=0; i<other_measure.notes.size(); i++)
+		notes.push_back(other_measure.notes[i]);
+}
+
+
+void Measure::prepend(const Measure &other_measure)
+{
+	Measure result_measure;
+	result_measure.append(other_measure);
+	result_measure.append(*this);
+	notes = result_measure.notes;
+}
+
+
+void Measure::invert(int axis)
+{
+	for (unsigned i=0; i<notes.size(); i++)
+		notes[i]->scale_degree = (notes[i]->scale_degree - axis) * -1 + axis;
+}
+
+
+void Measure::double_duration()
+{
+	// warning, there need to be limits in this function
+	for (unsigned i=0; i<notes.size(); i++)
+		notes[i]->duration /= 2;
+}
+
+
+void Measure::half_duration()
+{
+	// warning, there need to be limits in this function
+	for (unsigned i=0; i<notes.size(); i++)
+		notes[i]->duration *= 2;
+}
+
+
+
