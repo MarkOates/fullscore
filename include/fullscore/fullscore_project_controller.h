273464bf628cb63b8148e0ec72ab75dcee39e9eb
Mark Oates <euph_boy@hotmail.com>
2016-08-28 00:28:55 -0400

    Create FullscoreProjectController; move logic there

---
+++ include/fullscore/fullscore_project_controller.h
@@ -0,0 +1,38 @@
+#ifndef __FULLSCORE_FULLSCORE_PROJECT_CONTROLLER
+#define __FULLSCORE_FULLSCORE_PROJECT_CONTROLLER
+
+
+
+
+#include <allegro_flare/screens/simple_notification_screen.h>
+
+#include <fullscore/controller_bar.h>
+#include <fullscore/gui_score_editor.h>
+#include <fullscore/mixer.h>
+#include <fullscore/playback_device_generic.h>
+
+
+
+
+class FullscoreProjectController : public UIScreen
+{
+public:
+   SimpleNotificationScreen *simple_notification_screen;
+
+   GUIScoreEditor *score_editor;
+   UIControllerBar *gui_controller_bar;
+   UIMixer *gui_mixer;
+   UIFramedWindow *help_window;
+   bool showing_help_menu;
+
+   FullscoreProjectController(Display *display);
+   void create_help_window();
+   void primary_timer_func() override;
+   void key_down_func() override;
+   void on_message(UIWidget *sender, std::string message) override;
+};
+
+
+
+
+#endif
