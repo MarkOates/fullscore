60b2a39d5bf83c016ea62652dde2042d8a99aebb
Mark Oates <euph_boy@hotmail.com>
2016-08-28 00:02:27 -0400

    Move mixer to separate file

---
+++ include/fullscore/mixer.h
@@ -0,0 +1,44 @@
+#ifndef __FULLSCORE_UI_MIXER
+#define __FULLSCORE_UI_MIXER
+
+
+
+
+#include <allegro_flare/allegro_flare.h>
+#include <allegro_flare/gui/widget.h>
+
+
+
+
+class GUIMixer : public UIFramedWindow
+{
+public:
+	class Channel
+	{
+	public:
+		int channel_num;
+		int patch;
+		Channel(int channel_num, int patch);
+	};
+
+private:
+	class GUIPatchTextInput : public UITextInput
+	{
+	public:
+		int channel_num;
+		GUIPatchTextInput(UIWidget *parent, int channel_num, float x, float y);
+		void on_change() override;
+	};
+
+	std::vector<Channel> channels;
+
+public:
+	GUIMixer(UIWidget *parent, float x_, float y_, int num_channels=8);
+	Channel *get_channel(int channel_num);
+	int get_patch_num(int channel_num);
+};
+
+
+
+
+#endif
