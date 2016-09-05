b1c600c2f30c68abd8ec278037b8a6713b5e20f2
Mark Oates <euph_boy@hotmail.com>
2016-09-05 01:05:13 -0400

    Fix argument bounds checking on EraseNote.transform()

--- src/transforms/erase_note_transform.cpp
+++ src/transforms/erase_note_transform.cpp
@@ -27,7 +27,7 @@ Transform::EraseNote::~EraseNote()
 
 std::vector<Note> Transform::EraseNote::transform(std::vector<Note> notes)
 {
-   if (index_num < 0 || notes.size() >= index_num) return notes;
+   if (index_num < 0 || index_num >= notes.size()) return notes;
    notes.erase(notes.begin() + index_num);
    return notes;
 }
