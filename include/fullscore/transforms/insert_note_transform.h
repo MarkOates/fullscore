892479c75fc15318e6dff2765c96b287423074a8
Mark Oates <euph_boy@hotmail.com>
2016-09-07 22:30:47 -0400

    Add Transform::InsertNote

---
+++ include/fullscore/transforms/insert_note_transform.h
@@ -0,0 +1,31 @@
+#ifndef __FULLSCORE_INSERT_NOTE_TRANSFORM_HEADER
+#define __FULLSCORE_INSERT_NOTE_TRANSFORM_HEADER
+
+
+
+
+#include <fullscore/transform_base.h>
+
+#include <fullscore/note.h>
+
+
+
+
+namespace Transform
+{
+   class InsertNote : public Base
+   {
+   public:
+      int position;
+      Note note;
+
+      InsertNote(int position, Note note);
+      ~InsertNote();
+      virtual std::vector<Note> transform(std::vector<Note> n) override;
+   };
+}
+
+
+
+
+#endif
---
+++ src/transforms/insert_note_transform.cpp
@@ -0,0 +1,37 @@
+
+
+
+
+#include <fullscore/transforms/insert_note_transform.h>
+
+#include <allegro_flare/useful.h>
+
+
+
+
+Transform::InsertNote::InsertNote(int position, Note note)
+   : position(position)
+   , note(note)
+{
+}
+
+
+
+
+Transform::InsertNote::~InsertNote()
+{
+}
+
+
+
+
+std::vector<Note> Transform::InsertNote::transform(std::vector<Note> notes)
+{
+   position = limit<int>(0, notes.size()-1, position);
+	notes.insert(notes.begin() + position, note);
+   return notes;
+}
+
+
+
+
