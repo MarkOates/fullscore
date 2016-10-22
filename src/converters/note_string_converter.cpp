6e4f0fead631b656e829c799fbec16dc0de69575
Mark Oates <euph_boy@hotmail.com>
2016-10-22 14:52:36 -0400

    Add scaffold NoteStringConverter class

---
+++ src/converters/note_string_converter.cpp
@@ -0,0 +1,34 @@
+
+
+
+
+#include <fullscore/converters/note_string_converter.h>
+
+
+
+
+NoteStringConverter::NoteStringConverter(Note *note)
+   : note(note)
+{}
+
+
+
+
+bool NoteStringConverter::read(std::string)
+{
+   // unimplemented
+   return false;
+}
+
+
+
+
+std::string NoteStringConverter::write()
+{
+   // unimplemented
+   return "";
+}
+
+
+
+
