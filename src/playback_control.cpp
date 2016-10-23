818d2dc5303e3ae07b18e95f54afbe69992d62e2
Mark Oates <euph_boy@hotmail.com>
2016-10-23 11:24:28 -0400

    Improve formatting of playback_control.cpp

--- src/playback_control.cpp
+++ src/playback_control.cpp
@@ -1,6 +1,7 @@
 
 
 
+
 #include <fullscore/playback_control.h>
 
 #include <fullscore/models/note.h>
@@ -8,39 +9,38 @@
 
 
 
-
 class PitchTransform
 {
 public:
-	static int diatonic_in_c(int scale_degree, int accidental)
-		// given a diatonic scale degree (zero-based index, so scale
-		// degree ^1 is pitch==0, scale degree ^4 is pitch==3 and so on)
-		// returns the current MIDI pitch where pitch==0 is MIDDLE_C
-	{
-		int octave = 0;
-		while (scale_degree < 0) { scale_degree += 7; octave--; }
-		while (scale_degree >= 7) { scale_degree -= 7; octave++; }
-		int normalized_scale_degree = scale_degree % 7;
+   static int diatonic_in_c(int scale_degree, int accidental)
+      // given a diatonic scale degree (zero-based index, so scale
+      // degree ^1 is pitch==0, scale degree ^4 is pitch==3 and so on)
+      // returns the current MIDI pitch where pitch==0 is MIDDLE_C
+   {
+      int octave = 0;
+      while (scale_degree < 0) { scale_degree += 7; octave--; }
+      while (scale_degree >= 7) { scale_degree -= 7; octave++; }
+      int normalized_scale_degree = scale_degree % 7;
 
-		switch(normalized_scale_degree)
-		{
-		case 0: normalized_scale_degree = 0; break;
-		case 1: normalized_scale_degree = 2; break;
-		case 2: normalized_scale_degree = 4; break;
-		case 3: normalized_scale_degree = 5; break;
-		case 4: normalized_scale_degree = 7; break;
-		case 5: normalized_scale_degree = 9; break;
-		case 6: normalized_scale_degree = 11; break;
-		default: break;
-		}
+      switch(normalized_scale_degree)
+      {
+         case 0: normalized_scale_degree = 0; break;
+         case 1: normalized_scale_degree = 2; break;
+         case 2: normalized_scale_degree = 4; break;
+         case 3: normalized_scale_degree = 5; break;
+         case 4: normalized_scale_degree = 7; break;
+         case 5: normalized_scale_degree = 9; break;
+         case 6: normalized_scale_degree = 11; break;
+         default: break;
+      }
 
-		return normalized_scale_degree + octave * 12 + accidental;
-	}
-	static int diatonic_in_c_alto_clef(int scale_degree, int accidental)
-	{
-		// this translates it so that what is on the staff is in treble_clef
-		return diatonic_in_c(scale_degree, accidental);
-	}
+      return normalized_scale_degree + octave * 12 + accidental;
+   }
+   static int diatonic_in_c_alto_clef(int scale_degree, int accidental)
+   {
+      // this translates it so that what is on the staff is in treble_clef
+      return diatonic_in_c(scale_degree, accidental);
+   }
 };
 
 
@@ -48,12 +48,12 @@ public:
 
 
 PlaybackControl::PlaybackControl(MeasureGrid *measure_grid, PlaybackDeviceInterface *playback_device)
-	: position(0)
-	, playing(false)
-	, tempo_bpm(120)
-	, tempo_duration(4) // quarter note
-	, measure_grid(measure_grid)
-	, playback_device(playback_device)
+   : position(0)
+   , playing(false)
+   , tempo_bpm(120)
+   , tempo_duration(4) // quarter note
+   , measure_grid(measure_grid)
+   , playback_device(playback_device)
 {}
 
 
@@ -61,25 +61,25 @@ PlaybackControl::PlaybackControl(MeasureGrid *measure_grid, PlaybackDeviceInterf
 
 void PlaybackControl::reset()
 {
-	position = 0;
-	playing = false;
-	if (!measure_grid) return;
+   position = 0;
+   playing = false;
+   if (!measure_grid) return;
 
-	// reset all the notes
-	//     it would be nice if it could look something more like this:
-	//     ("SELECT * FROM NOTES WHERE score_id=current").each( { |note| note.on=false; note.off=false; } );
-	// but alas...
-	for (int x=0; x<measure_grid->get_num_measures(); x++)
-		for (int y=0; y<measure_grid->get_num_staves(); y++)
-		{
-			Measure *measure = measure_grid->get_measure(x, y);
-			for (unsigned n=0; n<measure->notes.size(); n++)
-			{
-				Note &note = measure->notes[n];
-				note.playback_info.attacked = false;
-				note.playback_info.released = false;
-			}
-		}
+   // reset all the notes
+   //     it would be nice if it could look something more like this:
+   //     ("SELECT * FROM NOTES WHERE score_id=current").each( { |note| note.on=false; note.off=false; } );
+   // but alas...
+   for (int x=0; x<measure_grid->get_num_measures(); x++)
+      for (int y=0; y<measure_grid->get_num_staves(); y++)
+      {
+         Measure *measure = measure_grid->get_measure(x, y);
+         for (unsigned n=0; n<measure->notes.size(); n++)
+         {
+            Note &note = measure->notes[n];
+            note.playback_info.attacked = false;
+            note.playback_info.released = false;
+         }
+      }
 }
 
 
@@ -87,38 +87,38 @@ void PlaybackControl::reset()
 
 void PlaybackControl::update(double time_now)
 {
-	if (!playing) return;
+   if (!playing) return;
 
-	float CURRENT_TIMER_BPM = 60.0f;
-	position += (tempo_bpm / 60.0f / CURRENT_TIMER_BPM / tempo_duration);
+   float CURRENT_TIMER_BPM = 60.0f;
+   position += (tempo_bpm / 60.0f / CURRENT_TIMER_BPM / tempo_duration);
 
-	// cycle through the notes, if they're past the attack points, then attack or release them
-	for (int x=0; x<measure_grid->get_num_measures(); x++)
-		for (int y=0; y<measure_grid->get_num_staves(); y++)
-		{
-			Measure *measure = measure_grid->get_measure(x, y);
-			for (unsigned n=0; n<measure->notes.size(); n++)
-			{
-				Note &note = measure->notes[n];
-				if (note.playback_info.released) continue;
+   // cycle through the notes, if they're past the attack points, then attack or release them
+   for (int x=0; x<measure_grid->get_num_measures(); x++)
+      for (int y=0; y<measure_grid->get_num_staves(); y++)
+      {
+         Measure *measure = measure_grid->get_measure(x, y);
+         for (unsigned n=0; n<measure->notes.size(); n++)
+         {
+            Note &note = measure->notes[n];
+            if (note.playback_info.released) continue;
 
-				if (!note.playback_info.attacked && position >= note.playback_info.start_time)
-				{
-					// attack the note
-					note.playback_info.attacked = true;
-					if (playback_device && !note.is_rest)
-						playback_device->note_on(y, PitchTransform::diatonic_in_c_alto_clef(note.scale_degree, note.accidental), 127);
-				}
+            if (!note.playback_info.attacked && position >= note.playback_info.start_time)
+            {
+               // attack the note
+               note.playback_info.attacked = true;
+               if (playback_device && !note.is_rest)
+                  playback_device->note_on(y, PitchTransform::diatonic_in_c_alto_clef(note.scale_degree, note.accidental), 127);
+            }
 
-				if (note.playback_info.attacked && position >= note.playback_info.end_time)
-				{
-					// release the note
-					note.playback_info.released = true;
-					if (playback_device && !note.is_rest)
-						playback_device->note_off(y, PitchTransform::diatonic_in_c_alto_clef(note.scale_degree, note.accidental));
-				}
-			}
-		}
+            if (note.playback_info.attacked && position >= note.playback_info.end_time)
+            {
+               // release the note
+               note.playback_info.released = true;
+               if (playback_device && !note.is_rest)
+                  playback_device->note_off(y, PitchTransform::diatonic_in_c_alto_clef(note.scale_degree, note.accidental));
+            }
+         }
+      }
 }
 
 
@@ -126,20 +126,20 @@ void PlaybackControl::update(double time_now)
 
 void PlaybackControl::refresh_note_start_and_end_times()
 {
-	// cycle through the notes
-	for (int x=0; x<measure_grid->get_num_measures(); x++)
-		for (int y=0; y<measure_grid->get_num_staves(); y++)
-		{
-			Measure *measure = measure_grid->get_measure(x, y);
-			float x_cursor = 0;
-			for (unsigned n=0; n<measure->notes.size(); n++)
-			{
-				Note &note = measure->notes[n];
-				note.playback_info.start_time = x_cursor + x;
-				note.playback_info.end_time = note.playback_info.start_time + note.get_duration_width();
-				x_cursor += note.get_duration_width();
-			}
-		}
+   // cycle through the notes
+   for (int x=0; x<measure_grid->get_num_measures(); x++)
+      for (int y=0; y<measure_grid->get_num_staves(); y++)
+      {
+         Measure *measure = measure_grid->get_measure(x, y);
+         float x_cursor = 0;
+         for (unsigned n=0; n<measure->notes.size(); n++)
+         {
+            Note &note = measure->notes[n];
+            note.playback_info.start_time = x_cursor + x;
+            note.playback_info.end_time = note.playback_info.start_time + note.get_duration_width();
+            x_cursor += note.get_duration_width();
+         }
+      }
 }
 
 
@@ -147,8 +147,11 @@ void PlaybackControl::refresh_note_start_and_end_times()
 
 void PlaybackControl::toggle_playback()
 {
-	refresh_note_start_and_end_times();
-	playing = !playing;
-	if (!playing) playback_device->all_notes_off();
+   refresh_note_start_and_end_times();
+   playing = !playing;
+   if (!playing) playback_device->all_notes_off();
 }
+
+
+
 
