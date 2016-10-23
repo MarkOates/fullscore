a4fca26096c6879f1d84ea3405f8d8c48a3c0d7c
Mark Oates <euph_boy@hotmail.com>
2016-10-23 11:49:04 -0400

    Add Action::ResetPlayback

---
+++ src/actions/reset_playback_action.cpp
@@ -0,0 +1,39 @@
+
+
+
+
+#include <fullscore/actions/reset_playback_action.h>
+
+#include <fullscore/gui_score_editor.h>
+
+
+
+
+Action::ResetPlayback::ResetPlayback(GUIScoreEditor *score_editor)
+   : Base("reset_playback")
+   , score_editor(score_editor)
+{
+}
+
+
+
+
+Action::ResetPlayback::~ResetPlayback()
+{
+}
+
+
+
+
+bool Action::ResetPlayback::execute()
+{
+   if (!score_editor) return false;
+
+   // unimplemented
+   return false;
+}
+
+
+
+
+
