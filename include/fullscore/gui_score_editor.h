a5a4d7f6723b2a480ec431c7db58e0bc6d2f1ef1
Mark Oates <euph_boy@hotmail.com>
2016-10-24 10:29:56 -0400

    Fix formatting in gui_score_editor.h

--- include/fullscore/gui_score_editor.h
+++ include/fullscore/gui_score_editor.h
@@ -4,40 +4,39 @@
 
 
 
-#include <allegro_flare/placement2d.h>
-#include <allegro_flare/display.h>
-
 #include <allegro_flare/gui/widget.h>
-
+#include <allegro_flare/display.h>
+#include <allegro_flare/placement2d.h>
 #include <fullscore/models/measure_grid.h>
-#include <fullscore/playback_control.h>
 #include <fullscore/music_engraver.h>
+#include <fullscore/playback_control.h>
+
 
 
 
 class GUIScoreEditor : public UIWidget
 {
 public:
-	MeasureGrid measure_grid;
-	PlaybackControl playback_control;
+   MeasureGrid measure_grid;
+   PlaybackControl playback_control;
 
-	int measure_cursor_x; // should be renamed to grid_cursor_x, grid_cursor_y
-	int measure_cursor_y;
+   int measure_cursor_x; // should be renamed to grid_cursor_x, grid_cursor_y
+   int measure_cursor_y;
    int note_cursor_x;
    bool input_mode;
 
-	MusicEngraver music_engraver;
+   MusicEngraver music_engraver;
 
-	bool showing_debug_data;
+   bool showing_debug_data;
 
-	float STAFF_HEIGHT;
-	float FULL_MEASURE_WIDTH;
+   float STAFF_HEIGHT;
+   float FULL_MEASURE_WIDTH;
 
-	GUIScoreEditor(UIWidget *parent, Display *display, PlaybackDeviceInterface *playback_device);
-	void on_draw() override;
-	void on_timer() override;
-	Measure *get_measure_at_cursor();
-	Note *get_note_at_cursor();
+   GUIScoreEditor(UIWidget *parent, Display *display, PlaybackDeviceInterface *playback_device);
+   void on_draw() override;
+   void on_timer() override;
+   Measure *get_measure_at_cursor();
+   Note *get_note_at_cursor();
 
    int move_measure_cursor_x(int delta);
    int move_measure_cursor_y(int delta);
@@ -57,4 +56,3 @@ public:
 
 
 #endif
-
