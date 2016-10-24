0611c8b985b377fa3398d8c52f2d4bdee57b6658
Mark Oates <euph_boy@hotmail.com>
2016-10-23 23:05:43 -0400

    Copy MeasureGrid.save() to MeasureGridFileConverter.save()

--- src/converters/measure_grid_file_converter.cpp
+++ src/converters/measure_grid_file_converter.cpp
@@ -4,6 +4,9 @@
 
 #include <fullscore/converters/measure_grid_file_converter.h>
 
+#include <allegro_flare/data_attr.h>
+#include <allegro_flare/useful_php.h>
+#include <fullscore/converters/note_string_converter.h>
 #include <fullscore/models/measure_grid.h>
 
 
@@ -22,8 +25,40 @@ bool MeasureGridFileConverter::save()
    if (!measure_grid) return false;
    if (filename.empty()) return false;
 
-   // unimplemented
-   return false;
+   DataAttr state;
+
+   state.set("grid_height", tostring(measure_grid->get_num_staves()));
+   state.set("grid_width", tostring(measure_grid->get_num_measures()));
+   state.set("file_format_version", "v0.0.1");
+
+   for (int y=0; y<measure_grid->get_num_staves(); y++)
+      for (int x=0; x<measure_grid->get_num_measures(); x++)
+      {
+         Measure *measure = measure_grid->get_measure(x, y);
+         if (!measure || measure->notes.empty()) continue;
+         std::string val = "";
+         std::vector<std::string> notes_strs;
+         for (int n=0; n<(int)measure->notes.size(); n++)
+         {
+            // grab the note
+            Note *note = measure->operator[](n);
+
+            // build the note into a string
+            NoteStringConverter note_string_converter(note);
+            std::string note_as_str = note_string_converter.write();
+
+            // put it in the stack
+            notes_strs.push_back(note_as_str);
+         }
+
+         // collapse the note-string
+         val = php::implode(";", notes_strs);
+
+         // put it in the state
+         state.set(tostring(x) + "," + tostring(y), val);
+      }
+
+   return state.save(filename);
 }
 
 
