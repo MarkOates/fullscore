bbc4b498291c042a7c61d4cb2232131909e8c013
Mark Oates <euph_boy@hotmail.com>
2016-10-23 21:25:20 -0400

    Simplify implementation of transposing down in AppController

--- src/fullscore_application_controller.cpp
+++ src/fullscore_application_controller.cpp
@@ -121,15 +121,7 @@ void FullscoreApplicationController::key_down_func()
       case ALLEGRO_KEY_S:
          // transpose down
          {
-            // create the transformation
-            Transform::Transpose transpose_transform(0);
-
-            // find the amount of the transformation
-            if (Framework::key_shift) transpose_transform.transposition = -7;
-            else if (Framework::key_ctrl); // nothing (this add sharps to the whole measure? .. probably not a feature to have)
-            else transpose_transform.transposition = -1;
-
-            // assign the transformation
+            Transform::Transpose transpose_transform(Framework::key_shift ? -7 : -1);
             transform = &transpose_transform;
          }
          break;
