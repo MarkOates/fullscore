3dbc0e1ee38ceb454aff8b8be0cec4669b706c88
Mark Oates <euph_boy@hotmail.com>
2016-10-23 23:14:14 -0400

    Copy MeasureGrid.load() to MeasureGridFileConverter.load()

--- src/converters/measure_grid_file_converter.cpp
+++ src/converters/measure_grid_file_converter.cpp
@@ -69,8 +69,60 @@ bool MeasureGridFileConverter::load()
    if (!measure_grid) return false;
    if (filename.empty()) return false;
 
-   // unimplemented
-   return false;
+   DataAttr state;
+   if (!state.load(filename)) return false;
+
+   // clear out the contents of the current measure-grid
+   measure_grid->voices.clear(); // TODO: fix this: mkay, this will create dangling pointers of Note* in the measures
+
+   // check the format version
+   std::string file_format_version = state.get("file_format_version");
+   if (file_format_version != "v0.0.1")
+   {
+      std::cout << "[MeasureGrid::load(std::string filename)] could not load this format version" << std::endl;
+      return false;
+   }
+
+   // the the size of the board, and resize the current measure-grid
+   int grid_height = atoi(state.get("grid_height").c_str());
+   int grid_width = atoi(state.get("grid_width").c_str());
+   measure_grid->voices.resize(grid_height, MeasureGrid::Row(grid_width));
+
+   // for now, remove those two elements.  The rest of the data in `state` is measure data
+   state.remove("grid_height");
+   state.remove("grid_width");
+   state.remove("file_format_version");
+
+   // get the
+   std::map<std::string, std::string> data = state.get_copy();
+   for (std::map<std::string, std::string>::iterator it = data.begin(); it!=data.end(); it++)
+   {
+      // get the measure x, and y (in the format "x,y")
+      std::vector<std::string> parts = php::explode(",", it->first);
+      if (parts.size() != 2) continue;
+      Measure *measure = measure_grid->get_measure(atoi(parts[0].c_str()), atoi(parts[1].c_str()));
+
+      // get the notes
+      std::vector<std::string> notes = php::explode(";", it->second);
+
+      for (unsigned i=0; i<notes.size(); i++)
+      {
+         // create a new note
+         Note new_note = Note();
+
+         // set the note from the string
+         NoteStringConverter note_string_reader(&new_note);
+         if (!note_string_reader.read(notes[i]))
+         {
+            std::cout << "[Error:] A note could not successfully be created from the parsed data" << std::endl;
+         }
+
+         // put the note into the measure
+         measure->notes.push_back(new_note);
+      }
+   }
+
+   return true;
 }
 
 
