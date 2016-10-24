a00c8e879097a1ba2f46fb51a32911220f48a602
Mark Oates <euph_boy@hotmail.com>
2016-10-23 23:31:37 -0400

    Improve formatting in measure_grid.h

--- include/fullscore/models/measure_grid.h
+++ include/fullscore/models/measure_grid.h
@@ -3,8 +3,8 @@
 
 
 
-#include <vector>
 
+#include <vector>
 #include <fullscore/models/measure.h>
 #include <fullscore/models/time_signature.h>
 
@@ -14,37 +14,34 @@
 class MeasureGrid
 {
 private:
-	friend class MeasureGridFileConverter;
+   friend class MeasureGridFileConverter;
 
-	class Row
-	{
-	public:
-		std::vector<Measure> measures;
-		Row(int num_measures);
-		Measure &operator[](unsigned int index);
-	};
+   class Row
+   {
+      public:
+         std::vector<Measure> measures;
+         Row(int num_measures);
+         Measure &operator[](unsigned int index);
+   };
 
-	std::vector<Row> voices;
-	std::vector<TimeSignature> time_signatures;
-
+   std::vector<Row> voices;
+   std::vector<TimeSignature> time_signatures;
 
 public:
-	MeasureGrid(int num_x_measures, int num_y_staves);
-	Measure *get_measure(int x_measure, int y_staff);
+   MeasureGrid(int num_x_measures, int num_y_staves);
+   Measure *get_measure(int x_measure, int y_staff);
 
-	int get_num_staves();
-	int get_num_measures();
+   int get_num_staves();
+   int get_num_measures();
 
-	void insert_staff(int index);
-	bool delete_staff(int index);
-	void append_staff();
+   void insert_staff(int index);
+   bool delete_staff(int index);
+   void append_staff();
 
-	void insert_measure(int index);
-	bool delete_measure(int index);
-	void append_measure();
+   void insert_measure(int index);
+   bool delete_measure(int index);
+   void append_measure();
 };
-
-
 
 
 
