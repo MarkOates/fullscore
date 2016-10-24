45a55c7fbafca23ff28483b7be6c8a26da720895
Mark Oates <euph_boy@hotmail.com>
2016-10-23 20:40:59 -0400

    Add implementation of Transform::AddDot

--- src/transforms/add_dot_transform.cpp
+++ src/transforms/add_dot_transform.cpp
@@ -23,7 +23,9 @@ Transform::AddDot::~AddDot()
 
 std::vector<Note> Transform::AddDot::transform(std::vector<Note> n)
 {
-   // unimplemented
+   for (auto &note : n)
+      note.dots = std::min(2, note.dots+1);
+
    return n;
 }
 
