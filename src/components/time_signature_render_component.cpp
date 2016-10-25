97244e1595252a1fe025c312f451f5b60e64b22e
Mark Oates <euph_boy@hotmail.com>
2016-10-24 23:19:17 -0400

    Implement simple TimeSignatureRenderComponent::render()

--- src/components/time_signature_render_component.cpp
+++ src/components/time_signature_render_component.cpp
@@ -4,7 +4,9 @@
 
 #include <fullscore/components/time_signature_render_component.h>
 
+#include <sstream>
 #include <fullscore/models/time_signature.h>
+#include <allegro_flare/framework.h>
 
 
 
@@ -24,7 +26,12 @@ TimeSignatureRenderComponent::~TimeSignatureRenderComponent()
 
 void TimeSignatureRenderComponent::render(float x, float y)
 {
-   // unimplemented
+   std::stringstream numerator, denominator;
+   numerator << time_signature->numerator;
+   denominator << time_signature->denominator_duration;
+
+   al_draw_text(Framework::font("DroidSans.ttf 20"), color::black, x, y-8, ALLEGRO_ALIGN_CENTER, numerator.str().c_str());
+   al_draw_text(Framework::font("DroidSans.ttf 20"), color::black, x, y+8, ALLEGRO_ALIGN_CENTER, denominator.str().c_str());
 }
 
 
