0baff8d5d8653469a3961850c6143bb69e5b44bb
Mark Oates <euph_boy@hotmail.com>
2016-10-26 09:02:43 -0400

    Post a notification when SaveMeasureGridAction fails

--- src/fullscore_application_controller.cpp
+++ src/fullscore_application_controller.cpp
@@ -249,6 +249,8 @@ void FullscoreApplicationController::key_down_func()
             SaveMeasureGridAction save_measure_grid_action(&score_editor->measure_grid, "score_filename.fs");
             if (save_measure_grid_action.execute())
                simple_notification_screen->spawn_notification("saved score as \"score_filename.fs\"");
+            else
+               simple_notification_screen->spawn_notification("ERROR: there was a problem saving \"score_filename.fs\"");
          }
          break;
       case ALLEGRO_KEY_F8:
