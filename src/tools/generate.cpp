a556f7f52140e91a00f59d1de7a994fb4a3772b5
Mark Oates <euph_boy@hotmail.com>
2016-10-27 22:13:38 -0400

    Capture program args into vector

--- src/tools/generate.cpp
+++ src/tools/generate.cpp
@@ -3,12 +3,16 @@
 
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 
 
-
-int main(int, char**)
+int main(int argv, char **argc)
 {
+   std::vector<std::string> args;
+   for (int i=0; i<argv; i++) args.push_back(argc[i]);
+
    std::cout << "WORKING!" << std::endl;
    return 0;
 }
