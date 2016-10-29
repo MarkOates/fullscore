885b3f1b52e0307bb4f2c5c925a68aa1f7f3a07c
Mark Oates <euph_boy@hotmail.com>
2016-10-29 17:31:27 -0400

    Move normal mode editing code into separate function

--- src/fullscore_application_controller.cpp
+++ src/fullscore_application_controller.cpp
@@ -85,6 +85,239 @@ void FullscoreApplicationController::primary_timer_func()
 
 void FullscoreApplicationController::execute_normal_mode_action_for_key(int al_keycode)
 {
+   //
+   // SCORE EDITING COMMANDS
+   //
+
+   std::vector<Note> *notes = nullptr;
+   Note *single_note = nullptr;
+   std::vector<Note> single_note_as_array;
+
+   if (score_editor->is_measure_mode())
+   {
+      Measure *focused_measure = score_editor->get_measure_at_cursor();
+      if (focused_measure) notes = &focused_measure->notes;
+   }
+   else
+   {
+      single_note = score_editor->get_note_at_cursor();
+      if (single_note)
+      {
+         single_note_as_array.push_back(*single_note);
+         notes = &single_note_as_array;
+      }
+   }
+
+   // locate and build the appropriate transform
+   switch(Framework::current_event->keyboard.keycode)
+   {
+   case ALLEGRO_KEY_W:
+      {
+         Action::TransposeTransform transpose_transform_action(notes, Framework::key_shift ? 7 : 1);
+         transpose_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_S:
+      {
+         Action::TransposeTransform transpose_transform_action(notes, Framework::key_shift ? -7 : -1);
+         transpose_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_A:
+      {
+         Action::HalfDurationTransform half_duration_transform_action(notes);
+         half_duration_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_D:
+      {
+         Action::DoubleDurationTransform double_duration_transform_action(notes);
+         double_duration_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_R:
+      {
+         Action::ToggleRest toggle_rest_action(notes);
+         toggle_rest_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_E:
+      {
+         Action::EraseNote erase_note_action(notes, score_editor->note_cursor_x);
+         erase_note_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_I:
+      {
+         Action::Transform::Invert invert_action(0);
+         invert_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_G:
+      {
+         Action::Transform::Retrograde retrograde_action(notes);
+         retrograde_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_FULLSTOP:
+      {
+         Action::AddDotTransform add_dot_transform_action(notes);
+         add_dot_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_COMMA:
+      {
+         Action::RemoveDotTransform remove_dot_transform_action(notes);
+         remove_dot_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_N:
+      {
+         Action::InsertNoteTransform insert_note_transform_action(notes, score_editor->note_cursor_x, Note());
+         insert_note_transform_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_F1:
+      {
+         if (showing_help_menu)
+         {
+            // hide the help menu
+            Framework::motion().cmove_to(&help_window->place.position.x, -600, 0.4);
+            Framework::motion().cmove_to(&help_window->place.position.y, -100, 0.4);
+            Framework::motion().cmove_to(&help_window->place.rotation, -0.1, 0.4);
+            showing_help_menu = false;
+         }
+         else
+         {
+            // show the help menu
+            Framework::motion().cmove_to(&help_window->place.position.x, display->center(), 0.4);
+            Framework::motion().cmove_to(&help_window->place.position.y, display->middle(), 0.4);
+            Framework::motion().cmove_to(&help_window->place.rotation, 0, 0.4);
+            showing_help_menu = true;
+         }
+      }
+      break;
+   case ALLEGRO_KEY_F2:
+      {
+         // toggle showing the debug data on the editor
+         score_editor->showing_debug_data = !score_editor->showing_debug_data;
+      }
+      break;
+   case ALLEGRO_KEY_SPACE:
+      {
+         Action::TogglePlayback toggle_playback_action(score_editor, gui_mixer);
+         toggle_playback_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_Q:
+      {
+         Action::ResetPlayback reset_playback_action(score_editor);
+         reset_playback_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_F7:
+      {
+         Action::SaveMeasureGrid save_measure_grid_action(&score_editor->measure_grid, "score_filename.fs");
+         if (save_measure_grid_action.execute())
+            simple_notification_screen->spawn_notification("saved score as \"score_filename.fs\"");
+         else
+            simple_notification_screen->spawn_notification("ERROR: there was a problem saving \"score_filename.fs\"");
+      }
+      break;
+   case ALLEGRO_KEY_F8:
+      {
+         MeasureGridFileConverter measure_grid_file_converter(&score_editor->measure_grid, "score_filename.fs");
+         measure_grid_file_converter.load();
+         simple_notification_screen->spawn_notification("loaded score from \"score_filename.fs\"");
+      }
+      break;
+   case ALLEGRO_KEY_UP:
+      {
+         Framework::motion().cmove(&score_editor->place.position.y, 200, 0.4);
+      }
+      break;
+   case ALLEGRO_KEY_DOWN:
+      {
+         Framework::motion().cmove(&score_editor->place.position.y, -200, 0.4);
+      }
+      break;
+   case ALLEGRO_KEY_RIGHT:
+      {
+         Framework::motion().cmove(&score_editor->place.position.x, -200, 0.4);
+      }
+      break;
+   case ALLEGRO_KEY_LEFT:
+      {
+         Framework::motion().cmove(&score_editor->place.position.x, 200, 0.4);
+      }
+      break;
+   case ALLEGRO_KEY_EQUALS:
+      {
+         if (Framework::key_shift)
+         {
+            Framework::motion().cmove_to(&score_editor->place.scale.x, 1, 0.3);
+            Framework::motion().cmove_to(&score_editor->place.scale.y, 1, 0.3);
+         }
+         else
+         {
+            Framework::motion().cmove(&score_editor->place.scale.x, 0.1, 0.4);
+            Framework::motion().cmove(&score_editor->place.scale.y, 0.1, 0.4);
+         }
+      }
+      break;
+   case ALLEGRO_KEY_MINUS:
+      {
+         Framework::motion().cmove(&score_editor->place.scale.x, -0.1, 0.4);
+         Framework::motion().cmove(&score_editor->place.scale.y, -0.1, 0.4);
+      }
+      break;
+   case ALLEGRO_KEY_H:
+      {
+         Action::MoveCursorLeft move_cursor_left_action(score_editor);
+         move_cursor_left_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_J:
+      {
+         Action::MoveCursorDown move_cursor_down_action(score_editor);
+         move_cursor_down_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_K:
+      {
+         Action::MoveCursorUp move_cursor_up_action(score_editor);
+         move_cursor_up_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_L:
+      {
+         Action::MoveCursorRight move_cursor_right_action(score_editor);
+         move_cursor_right_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_Y:
+      {
+         Measure *focused_measure = score_editor->get_measure_at_cursor();
+         Action::YankMeasureToBuffer yank_measure_to_buffer_action(&yank_measure_buffer, focused_measure);
+         yank_measure_to_buffer_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_P:
+      {
+         Measure *destination_measure = score_editor->get_measure_at_cursor();
+         Action::PasteMeasureFromBuffer paste_measure_from_buffer_action(destination_measure, &yank_measure_buffer);
+         paste_measure_from_buffer_action.execute();
+      }
+      break;
+   case ALLEGRO_KEY_TAB:
+      score_editor->toggle_edit_mode_target();
+      break;
+   }
+
+   if (single_note && !single_note_as_array.empty())
+   {
+      *single_note = single_note_as_array.at(0);
+   }
 }
 
 
@@ -103,239 +336,6 @@ void FullscoreApplicationController::key_down_func()
    else if (!command_bar->text_input->is_focused())
    // while the command bar is NOT focused, here are the normal keyboard inputs
    {
-      //
-      // SCORE EDITING COMMANDS
-      //
-
-      std::vector<Note> *notes = nullptr;
-      Note *single_note = nullptr;
-      std::vector<Note> single_note_as_array;
-
-      if (score_editor->is_measure_mode())
-      {
-         Measure *focused_measure = score_editor->get_measure_at_cursor();
-         if (focused_measure) notes = &focused_measure->notes;
-      }
-      else
-      {
-         single_note = score_editor->get_note_at_cursor();
-         if (single_note)
-         {
-            single_note_as_array.push_back(*single_note);
-            notes = &single_note_as_array;
-         }
-      }
-
-      // locate and build the appropriate transform
-      switch(Framework::current_event->keyboard.keycode)
-      {
-      case ALLEGRO_KEY_W:
-         {
-            Action::TransposeTransform transpose_transform_action(notes, Framework::key_shift ? 7 : 1);
-            transpose_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_S:
-         {
-            Action::TransposeTransform transpose_transform_action(notes, Framework::key_shift ? -7 : -1);
-            transpose_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_A:
-         {
-            Action::HalfDurationTransform half_duration_transform_action(notes);
-            half_duration_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_D:
-         {
-            Action::DoubleDurationTransform double_duration_transform_action(notes);
-            double_duration_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_R:
-         {
-            Action::ToggleRest toggle_rest_action(notes);
-            toggle_rest_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_E:
-         {
-            Action::EraseNote erase_note_action(notes, score_editor->note_cursor_x);
-            erase_note_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_I:
-         {
-            Action::Transform::Invert invert_action(0);
-            invert_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_G:
-         {
-            Action::Transform::Retrograde retrograde_action(notes);
-            retrograde_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_FULLSTOP:
-         {
-            Action::AddDotTransform add_dot_transform_action(notes);
-            add_dot_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_COMMA:
-         {
-            Action::RemoveDotTransform remove_dot_transform_action(notes);
-            remove_dot_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_N:
-         {
-            Action::InsertNoteTransform insert_note_transform_action(notes, score_editor->note_cursor_x, Note());
-            insert_note_transform_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_F1:
-         {
-            if (showing_help_menu)
-            {
-               // hide the help menu
-               Framework::motion().cmove_to(&help_window->place.position.x, -600, 0.4);
-               Framework::motion().cmove_to(&help_window->place.position.y, -100, 0.4);
-               Framework::motion().cmove_to(&help_window->place.rotation, -0.1, 0.4);
-               showing_help_menu = false;
-            }
-            else
-            {
-               // show the help menu
-               Framework::motion().cmove_to(&help_window->place.position.x, display->center(), 0.4);
-               Framework::motion().cmove_to(&help_window->place.position.y, display->middle(), 0.4);
-               Framework::motion().cmove_to(&help_window->place.rotation, 0, 0.4);
-               showing_help_menu = true;
-            }
-         }
-         break;
-      case ALLEGRO_KEY_F2:
-         {
-            // toggle showing the debug data on the editor
-            score_editor->showing_debug_data = !score_editor->showing_debug_data;
-         }
-         break;
-      case ALLEGRO_KEY_SPACE:
-         {
-            Action::TogglePlayback toggle_playback_action(score_editor, gui_mixer);
-            toggle_playback_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_Q:
-         {
-            Action::ResetPlayback reset_playback_action(score_editor);
-            reset_playback_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_F7:
-         {
-            Action::SaveMeasureGrid save_measure_grid_action(&score_editor->measure_grid, "score_filename.fs");
-            if (save_measure_grid_action.execute())
-               simple_notification_screen->spawn_notification("saved score as \"score_filename.fs\"");
-            else
-               simple_notification_screen->spawn_notification("ERROR: there was a problem saving \"score_filename.fs\"");
-         }
-         break;
-      case ALLEGRO_KEY_F8:
-         {
-            MeasureGridFileConverter measure_grid_file_converter(&score_editor->measure_grid, "score_filename.fs");
-            measure_grid_file_converter.load();
-            simple_notification_screen->spawn_notification("loaded score from \"score_filename.fs\"");
-         }
-         break;
-      case ALLEGRO_KEY_UP:
-         {
-            Framework::motion().cmove(&score_editor->place.position.y, 200, 0.4);
-         }
-         break;
-      case ALLEGRO_KEY_DOWN:
-         {
-            Framework::motion().cmove(&score_editor->place.position.y, -200, 0.4);
-         }
-         break;
-      case ALLEGRO_KEY_RIGHT:
-         {
-            Framework::motion().cmove(&score_editor->place.position.x, -200, 0.4);
-         }
-         break;
-      case ALLEGRO_KEY_LEFT:
-         {
-            Framework::motion().cmove(&score_editor->place.position.x, 200, 0.4);
-         }
-         break;
-      case ALLEGRO_KEY_EQUALS:
-         {
-            if (Framework::key_shift)
-            {
-               Framework::motion().cmove_to(&score_editor->place.scale.x, 1, 0.3);
-               Framework::motion().cmove_to(&score_editor->place.scale.y, 1, 0.3);
-            }
-            else
-            {
-               Framework::motion().cmove(&score_editor->place.scale.x, 0.1, 0.4);
-               Framework::motion().cmove(&score_editor->place.scale.y, 0.1, 0.4);
-            }
-         }
-         break;
-      case ALLEGRO_KEY_MINUS:
-         {
-            Framework::motion().cmove(&score_editor->place.scale.x, -0.1, 0.4);
-            Framework::motion().cmove(&score_editor->place.scale.y, -0.1, 0.4);
-         }
-         break;
-      case ALLEGRO_KEY_H:
-         {
-            Action::MoveCursorLeft move_cursor_left_action(score_editor);
-            move_cursor_left_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_J:
-         {
-            Action::MoveCursorDown move_cursor_down_action(score_editor);
-            move_cursor_down_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_K:
-         {
-            Action::MoveCursorUp move_cursor_up_action(score_editor);
-            move_cursor_up_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_L:
-         {
-            Action::MoveCursorRight move_cursor_right_action(score_editor);
-            move_cursor_right_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_Y:
-         {
-            Measure *focused_measure = score_editor->get_measure_at_cursor();
-            Action::YankMeasureToBuffer yank_measure_to_buffer_action(&yank_measure_buffer, focused_measure);
-            yank_measure_to_buffer_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_P:
-         {
-            Measure *destination_measure = score_editor->get_measure_at_cursor();
-            Action::PasteMeasureFromBuffer paste_measure_from_buffer_action(destination_measure, &yank_measure_buffer);
-            paste_measure_from_buffer_action.execute();
-         }
-         break;
-      case ALLEGRO_KEY_TAB:
-         score_editor->toggle_edit_mode_target();
-         break;
-      }
-
-      if (single_note && !single_note_as_array.empty())
-      {
-         *single_note = single_note_as_array.at(0);
-      }
    }
 }
 
