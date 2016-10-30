8780735c5acaea71019b196b89490844c2722395
Mark Oates <euph_boy@hotmail.com>
2016-10-30 11:30:55 -0400

    Add tools/new_action.rb with some monkey patching

---
+++ tools/new_action.rb
@@ -0,0 +1,20 @@
+
+
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+
+  def camel_case
+    return self if self !~ /_/ && self =~ /[A-Z]+.*/
+    split('_').map{|e| e.capitalize}.join
+  end
+end
+
+
+
