c7ebe9945ae57f133fea8161a7f15ad53b6fa5bc
Mark Oates <euph_boy@hotmail.com>
2016-10-30 13:31:26 -0400

    Create InsertStaff action

---
+++ src/actions/insert_staff_action.cpp
@@ -0,0 +1,39 @@
+
+
+
+
+#include <fullscore/actions/insert_staff_action.h>
+
+#include <fullscore/models/measure_grid.h>
+
+
+
+
+Action::InsertStaff::InsertStaff(MeasureGrid *measure_grid, int at_index)
+   : Base("insert_staff")
+   , measure_grid(measure_grid)
+   , at_index(at_index)
+{}
+
+
+
+
+Action::InsertStaff::~InsertStaff()
+{}
+
+
+
+
+bool Action::InsertStaff::execute()
+{
+   if (!measure_grid) return false;
+   if (at_index < 0 || at_index >= measure_grid->get_num_staves()) return false;
+
+   measure_grid->insert_staff(at_index);
+
+   return true;
+}
+
+
+
+
