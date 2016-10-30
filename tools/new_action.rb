d587658cd7997bf02d0fb1b97b5d3ea3d40064a0
Mark Oates <euph_boy@hotmail.com>
2016-10-30 11:59:50 -0400

    Implement simple proceedure for filling templates for actions

--- tools/new_action.rb
+++ tools/new_action.rb
@@ -18,3 +18,33 @@ end
 
 
 
+# open the file
+source_template_file = File.open('tools/templates/__transform_action_template.cpp', 'r')
+header_template_file = File.open('tools/templates/__transform_action_template.h', 'r')
+
+
+# read the contents to string
+source_code = source_template_file.read
+header_code = header_template_file.read
+
+
+# overwrite the template tokens
+class_name = 'ThisIsClassName'
+source_code.gsub!(/CLASS_NAME/, class_name.camel_case)
+source_code.gsub!(/SNAKECASE_ACTION_NAME/, class_name.underscore)
+header_code.gsub!(/CLASS_NAME/, class_name.camel_case)
+header_code.gsub!(/SNAKECASE_ACTION_NAME/, class_name.underscore)
+
+
+# save the file with a new filename
+source_filename = "src/actions/transforms/#{class_name.underscore}_action.cpp"
+header_filename = "include/fullscore/actions/transforms/#{class_name.underscore}_action.h"
+IO.write(source_filename, source_code)
+IO.write(header_filename, header_code)
+
+
+# write `git commit -m "Create source files for CLASS_NAME action"`
+
+
+
+
