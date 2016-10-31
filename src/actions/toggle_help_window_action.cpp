456bbe99bdac7ffac98fcc46a65247aab3c81054
Mark Oates <euph_boy@hotmail.com>
2016-10-31 01:22:19 -0400

    Implement ToggleHelpWindow action

--- src/actions/toggle_help_window_action.cpp
+++ src/actions/toggle_help_window_action.cpp
@@ -4,6 +4,7 @@
 
 #include <fullscore/actions/toggle_help_window_action.h>
 
+#include <fullscore/fullscore_application_controller.h>
 
 
 
@@ -27,13 +28,23 @@ bool Action::ToggleHelpWindow::execute()
 {
    if (!motion || !fullscore_application_controller) return false;
 
+   placement2d *place = &fullscore_application_controller->help_window->place;
+
    if (fullscore_application_controller->showing_help_menu)
    {
       // hide the help menu
+      motion->cmove_to(&place->position.x, -600, 0.4);
+      motion->cmove_to(&place->position.y, -100, 0.4);
+      motion->cmove_to(&place->rotation, -0.1, 0.4);
+      fullscore_application_controller->showing_help_menu = false;
    }
    else
    {
       // show the help menu
+      motion->cmove_to(&place->position.x, fullscore_application_controller->display->center(), 0.4);
+      motion->cmove_to(&place->position.y, fullscore_application_controller->display->middle(), 0.4);
+      motion->cmove_to(&place->rotation, 0, 0.4);
+      fullscore_application_controller->showing_help_menu = true;
    }
 
    return false;
