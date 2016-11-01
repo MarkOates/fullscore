1c752054df42fd39d09b9cf871dd8a679690ed7c
Mark Oates <euph_boy@hotmail.com>
2016-11-01 03:01:05 -0400

    Add TransposeDown action

---
+++ src/actions/transforms/transpose_down_action.cpp
@@ -0,0 +1,44 @@
+
+
+
+
+#include <fullscore/actions/transforms/transpose_down_action.h>
+
+#include <fullscore/transforms/transpose_transform.h>
+#include <fullscore/models/note.h>
+
+
+
+
+Action::TransposeDown::TransposeDown(Note *note)
+   : Base("transpose")
+   , note(note)
+{}
+
+
+
+
+Action::TransposeDown::~TransposeDown()
+{}
+
+
+
+
+bool Action::TransposeDown::execute()
+{
+   if (!note) return false;
+
+   std::vector<Note> single_note_as_array;
+   single_note_as_array.push_back(*note);
+
+   Transform::Transpose transpose_transform(-1);
+   single_note_as_array = transpose_transform.transform(single_note_as_array);
+
+   *note = single_note_as_array.at(0);
+
+   return true;
+}
+
+
+
+
