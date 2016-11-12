bcb2cef0f98fe92d54d72bd1ca68e130cd82d36a
Mark Oates <euph_boy@hotmail.com>
2016-11-12 18:26:41 -0500

    Use cached value for get_real_measure_cursor_x

--- source/gui_score_editor.cpp
+++ source/gui_score_editor.cpp
@@ -76,36 +76,36 @@ void GUIScoreEditor::on_draw()
    // draw a hilight box under the focused measure
    Measure *measure = get_measure_at_cursor();
    Note *note = get_note_at_cursor();
+   float CACHED_get_measure_cursor_real_x = get_measure_cursor_real_x();
 
    if (get_measure_at_cursor())
    {
       float measure_width = get_measure_width(*measure) * FULL_MEASURE_WIDTH;
 
       // fill
-      al_draw_filled_rounded_rectangle(measure_cursor_x*FULL_MEASURE_WIDTH, measure_cursor_y*STAFF_HEIGHT,
-         measure_cursor_x*FULL_MEASURE_WIDTH+measure_width, measure_cursor_y*STAFF_HEIGHT+STAFF_HEIGHT,
+      al_draw_filled_rounded_rectangle(CACHED_get_measure_cursor_real_x, measure_cursor_y*STAFF_HEIGHT,
+         CACHED_get_measure_cursor_real_x+measure_width, measure_cursor_y*STAFF_HEIGHT+STAFF_HEIGHT,
          4, 4, color::color(color::aliceblue, 0.2));
 
       // outline
       if (is_measure_target_mode())
-         al_draw_rounded_rectangle(measure_cursor_x*FULL_MEASURE_WIDTH, measure_cursor_y*STAFF_HEIGHT,
-            measure_cursor_x*FULL_MEASURE_WIDTH+measure_width, measure_cursor_y*STAFF_HEIGHT+STAFF_HEIGHT,
+         al_draw_rounded_rectangle(CACHED_get_measure_cursor_real_x, measure_cursor_y*STAFF_HEIGHT,
+            CACHED_get_measure_cursor_real_x+measure_width, measure_cursor_y*STAFF_HEIGHT+STAFF_HEIGHT,
             4, 4, color::color(color::aliceblue, 0.7), 2.0);
    }
 
    // draw a hilight box under the focused note
    if (measure && note)
    {
-      float measure_cursor_real_x = get_measure_cursor_real_x();
       float measure_cursor_real_y = get_measure_cursor_real_y();
       float note_real_offset_x = get_measure_length_to_note(*measure, note_cursor_x) * FULL_MEASURE_WIDTH;
       float note_width = DurationHelper::get_length(note->duration, note->dots) * FULL_MEASURE_WIDTH;
 
       // fill
       al_draw_filled_rounded_rectangle(
-            measure_cursor_real_x + note_real_offset_x,
+            CACHED_get_measure_cursor_real_x + note_real_offset_x,
             measure_cursor_real_y,
-            measure_cursor_real_x + note_real_offset_x + note_width,
+            CACHED_get_measure_cursor_real_x + note_real_offset_x + note_width,
             measure_cursor_real_y + STAFF_HEIGHT,
             6,
             6,
@@ -115,9 +115,9 @@ void GUIScoreEditor::on_draw()
       // outline
       if (is_note_target_mode())
          al_draw_rounded_rectangle(
-               measure_cursor_real_x + note_real_offset_x,
+               CACHED_get_measure_cursor_real_x + note_real_offset_x,
                measure_cursor_real_y,
-               measure_cursor_real_x + note_real_offset_x + note_width,
+               CACHED_get_measure_cursor_real_x + note_real_offset_x + note_width,
                measure_cursor_real_y + STAFF_HEIGHT,
                6,
                6,
@@ -129,10 +129,9 @@ void GUIScoreEditor::on_draw()
    // draw the measure cursor
    if (measure_cursor_x >= 0 && measure_cursor_y >= 0)
    {
-      float measure_cursor_real_x = get_measure_cursor_real_x();
       float measure_cursor_real_y = get_measure_cursor_real_y();
-      al_draw_line(measure_cursor_real_x, measure_cursor_real_y,
-            measure_cursor_real_x, measure_cursor_real_y+STAFF_HEIGHT,
+      al_draw_line(CACHED_get_measure_cursor_real_x, measure_cursor_real_y,
+            CACHED_get_measure_cursor_real_x, measure_cursor_real_y+STAFF_HEIGHT,
             color::white, 3.0);
    }
 
