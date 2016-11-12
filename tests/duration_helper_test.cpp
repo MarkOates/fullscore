d77eb1eecee279c4e0e42ed2bc28176e3af02d49
Mark Oates <euph_boy@hotmail.com>
2016-11-12 16:08:59 -0500

    Add 2 extra length tests for DurationHelper

--- tests/duration_helper_test.cpp
+++ tests/duration_helper_test.cpp
@@ -21,6 +21,12 @@ TEST(DurationHelperTest, returns_the_length_of_a_time_signature)
    TimeSignature time_signature_c(3, 8, 0);
    EXPECT_EQ(DurationHelper::get_length(time_signature_c), 0.375);
 
+   TimeSignature time_signature_n(5, 2, 0);
+   EXPECT_EQ(DurationHelper::get_length(time_signature_n), 2.5);
+
+   TimeSignature time_signature_s(5, 16, 0);
+   EXPECT_EQ(DurationHelper::get_length(time_signature_s), 0.3125);
+
    TimeSignature time_signature_d(2, 4, 1);
    TimeSignature time_signature_e(6, 8, 0);
    EXPECT_EQ(DurationHelper::get_length(time_signature_d), 0.75);
