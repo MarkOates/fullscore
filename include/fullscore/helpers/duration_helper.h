093b0a60625403a9d6a84e3f6b6d8db843178902
Mark Oates <euph_boy@hotmail.com>
2016-11-11 21:05:20 -0500

    Add DurationHelper with get_length()

--- include/fullscore/helpers/duration_helper.h
+++ include/fullscore/helpers/duration_helper.h
@@ -1,1 +1,14 @@
 #pragma once
+
+
+
+
+class DurationHelper
+{
+public:
+   static float get_length(int duration, int dots);
+};
+
+
+
+
--- source/helpers/duration_helper.cpp
+++ source/helpers/duration_helper.cpp
@@ -7,3 +7,20 @@
 
 
 
+float DurationHelper::get_length(int duration, int dots)
+{
+   float width = 1.0f / duration;
+   float dots_percentage = 0.0f;
+   float previous_ammount = 1.0f;
+   for (int i=0; i<dots; i++)
+   {
+      previous_ammount *= 0.5f;
+      dots_percentage += previous_ammount;
+   }
+
+   return width + width * dots_percentage;
+}
+
+
+
+
