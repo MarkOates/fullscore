ebb287bc593dc31667047c1fa71179d0a0159c34
Mark Oates <euph_boy@hotmail.com>
2016-11-12 15:50:05 -0500

    Finish test in DurationHelperTest

--- tests/duration_helper_test.cpp
+++ tests/duration_helper_test.cpp
@@ -19,12 +19,13 @@ TEST(DurationHelperTest, returns_the_length_of_a_time_signature)
    EXPECT_EQ(DurationHelper::get_length(time_signature_b), 0.75);
 
    TimeSignature time_signature_c(3, 8, 0);
-   EXPECT_EQ(DurationHelper::get_length(time_signature_c), 0.75);
+   EXPECT_EQ(DurationHelper::get_length(time_signature_c), 0.375);
 
    TimeSignature time_signature_d(2, 4, 1);
    TimeSignature time_signature_e(6, 8, 0);
-   EXPECT_EQ(DurationHelper::get_length(time_signature_d), 0.375);
+   EXPECT_EQ(DurationHelper::get_length(time_signature_d), 0.75);
    EXPECT_EQ(DurationHelper::get_length(time_signature_d), DurationHelper::get_length(time_signature_e));
+   EXPECT_EQ(DurationHelper::get_length(time_signature_d), DurationHelper::get_length(time_signature_b));
 }
 
 
