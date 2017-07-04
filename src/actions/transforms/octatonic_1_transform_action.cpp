fe49658cf3dae2d317644fd861dedc321499545b
Mark Oates <euph_boy@hotmail.com>
2017-07-03 21:05:21 -0400

    Create Octatonic1Transform action

---
+++ src/actions/transforms/octatonic_1_transform_action.cpp
@@ -0,0 +1,34 @@
+
+
+
+#include <fullscore/actions/transforms/octatonic_1_transform_action.h>
+
+#include <fullscore/transforms/octatonic_1.h>
+#include <fullscore/models/note.h>
+
+
+
+Action::Octatonic1Transform::Octatonic1Transform(std::vector<Note> *notes)
+   : Base("octatonic_1_transform")
+   , notes(notes)
+{}
+
+
+
+Action::Octatonic1Transform::~Octatonic1Transform()
+{}
+
+
+
+bool Action::Octatonic1Transform::execute()
+{
+   if (!notes) return false;
+
+   ::Transform::Octatonic1 octatonic_1_transform;
+   *notes = octatonic_1_transform.transform(*notes);
+
+   return true;
+}
+
+
+
