de8e0eb13db2b954a9c4ac4aa9a799e6b9bcfe69
Mark Oates <euph_boy@hotmail.com>
2017-07-09 12:49:12 -0400

    Add MeasureGrid test for get_ and set_ voice_name

--- tests/measure_grid_test.cpp
+++ tests/measure_grid_test.cpp
@@ -15,6 +15,22 @@ TEST(MeasureGridTest, creates_successfully)
 
 
 
+TEST(MeasureGridTest, sets_and_gets_the_name_of_a_row)
+{
+   MeasureGrid measure_grid(1, 3);
+
+   EXPECT_EQ(true, measure_grid.set_voice_name(0, "Trombone"));
+   EXPECT_EQ("Trombone", measure_grid.get_voice_name(0));
+
+   EXPECT_EQ(true, measure_grid.set_voice_name(2, "Euphonium"));
+   EXPECT_EQ("Euphonium", measure_grid.get_voice_name(2));
+
+   EXPECT_EQ(true, measure_grid.set_voice_name(2, "Tuba"));
+   EXPECT_EQ("Tuba", measure_grid.get_voice_name(2));
+}
+
+
+
 int main(int argc, char **argv)
 {
    ::testing::InitGoogleTest(&argc, argv);
