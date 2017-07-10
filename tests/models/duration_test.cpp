c80f4a82f2ab8b100c5e68838cdd4a6f7cda4127
Mark Oates <euph_boy@hotmail.com>
2017-07-09 20:10:15 -0400

    Add equality operator tests for Duration

--- tests/models/duration_test.cpp
+++ tests/models/duration_test.cpp
@@ -14,6 +14,24 @@ TEST(DurationTest, can_be_created)
 
 
 
+TEST(DurationTest, equality_operator_returns_true_on_equal_durations)
+{
+   EXPECT_EQ(true, Duration(Duration::HALF, 0).operator==(Duration(Duration::HALF, 0)));
+   EXPECT_EQ(true, Duration(Duration::HALF, 1).operator==(Duration(Duration::HALF, 1)));
+   EXPECT_EQ(true, Duration(Duration::QUARTER, 1).operator==(Duration(Duration::QUARTER, 1)));
+}
+
+
+
+TEST(DurationTest, equality_operator_returns_false_on_unequal_durations)
+{
+   EXPECT_EQ(false, Duration(Duration::QUARTER, 0).operator==(Duration(Duration::HALF, 0)));
+   EXPECT_EQ(false, Duration(Duration::HALF, 0).operator==(Duration(Duration::HALF, 1)));
+   EXPECT_EQ(false, Duration(Duration::EIGHTH, 1).operator==(Duration(Duration::QUARTER, 2)));
+}
+
+
+
 int main(int argc, char **argv)
 {
    ::testing::InitGoogleTest(&argc, argv);
