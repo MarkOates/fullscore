b06eb8c33a941b73eab7efb57a13df8e4c3be91a
Mark Oates <euph_boy@hotmail.com>
2017-07-09 20:16:45 -0400

    Add test file for Pitch

---
+++ tests/models/pitch_test.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <gtest/gtest.h>
+
+#include <fullscore/models/pitch.h>
+
+
+
+TEST(PitchTest, can_be_created)
+{
+   Pitch pitch(0, 0);
+}
+
+
+
+TEST(PitchTest, equality_operator_returns_true_on_equal_pitch)
+{
+   EXPECT_EQ(true, Pitch(0, 0).operator==(Pitch(0, 0)));
+   EXPECT_EQ(true, Pitch(-2, 1).operator==(Pitch(-2, 1)));
+   EXPECT_EQ(true, Pitch(4, -1).operator==(Pitch(4, -1)));
+}
+
+
+
+TEST(PitchTest, equality_operator_returns_false_on_unequal_pitch)
+{
+   EXPECT_EQ(false, Pitch(0, 0).operator==(Pitch(1, 0)));
+   EXPECT_EQ(false, Pitch(-3, 0).operator==(Pitch(-3, 1)));
+   EXPECT_EQ(false, Pitch(14, 1).operator==(Pitch(-3, 2)));
+}
+
+
+
+int main(int argc, char **argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
+
+
+
