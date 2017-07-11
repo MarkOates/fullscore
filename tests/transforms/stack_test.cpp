eb22a4abe39e51ab3b0f32e7a23b6ed3c74482be
Mark Oates <euph_boy@hotmail.com>
2017-07-11 18:28:55 -0400

    Add test for Transform::Stack get_transformations()

--- tests/transforms/stack_test.cpp
+++ tests/transforms/stack_test.cpp
@@ -6,6 +6,8 @@
 #include <fullscore/transforms/stack.h>
 
 #include <fullscore/transforms/insert_note.h>
+#include <fullscore/transforms/retrograde.h>
+#include <fullscore/transforms/toggle_rest.h>
 
 
 
@@ -38,6 +40,28 @@ TEST(TransformStackTest, executes_the_sequence_of_transforms)
    std::vector<Note> returned_notes = transform_stack.transform(source_notes);
 
    EXPECT_EQ(expected_notes, returned_notes);
+}
+
+
+
+TEST(TransformStackTest, returns_a_list_of_transforms)
+{
+   Transform::Stack transform_stack;
+
+   Transform::InsertNote insert_note_transform(0, Note());
+   Transform::ToggleRest toggle_rest_transform;
+   Transform::Retrograde retrograde_transform;
+
+   transform_stack.add_transform(&insert_note_transform);
+   transform_stack.add_transform(&toggle_rest_transform);
+   transform_stack.add_transform(&retrograde_transform);
+
+   std::vector<Transform::Base *> transforms = transform_stack.get_transformations();
+
+   EXPECT_EQ(3, transforms.size());
+   EXPECT_EQ("insert_note", transforms[0]->get_identifier());
+   EXPECT_EQ("toggle_rest", transforms[1]->get_identifier());
+   EXPECT_EQ("retrograde", transforms[2]->get_identifier());
 }
 
 
