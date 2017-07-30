ef0d23de28505c0634daaccbde0cd339d7734a39
Mark Oates <euph_boy@hotmail.com>
2017-07-30 02:17:37 -0400

    Add MeasureGrid::Row functions to Staff::Base

--- include/fullscore/models/staves/base.h
+++ include/fullscore/models/staves/base.h
@@ -3,6 +3,11 @@
 
 
 #include <string>
+#include <vector>
+
+
+
+namespace Measure { class Base; }
 
 
 
@@ -14,12 +19,30 @@ namespace Staff
       std::string type;
       int id;
 
+      // MeasureGrid::Row attributes
+
+      std::string name;
+      std::vector<Measure::Base *> measures;
+
    public:
-      Base(std::string type);
+      Base(std::string type, int num_measures);
       virtual ~Base();
       std::string get_type();
       bool is_type(std::string type);
       int get_id();
+
+      // MeasureGrid::Row functions
+
+      void set_name(std::string name);
+      std::string get_name();
+
+      int get_num_measures();
+      bool set_measure(int measure_x, Measure::Base *measure);
+      bool insert_measure(int at_index, Measure::Base *measure);
+      bool erase_measure(int at_index);
+      bool append_measure(Measure::Base *measure);
+
+      Measure::Base *get_measure(int x_measure);
    };
 };
 
--- src/models/staves/base.cpp
+++ src/models/staves/base.cpp
@@ -1,15 +1,20 @@
 
 
 
+#include <fullscore/models/measures/base.h>
 #include <fullscore/models/staves/base.h>
 #include <fullscore/models/staff.h>
 
 
 
-Staff::Base::Base(std::string type)
+Staff::Base::Base(std::string type, int num_measures)
    : type(type)
    , id(Staff::next_id++)
-{}
+   , name()
+   , measures()
+{
+   for (unsigned i=0; i<num_measures; i++) measures.push_back(nullptr);
+}
 
 
 
@@ -35,6 +40,97 @@ bool Staff::Base::is_type(std::string type)
 int Staff::Base::get_id()
 {
    return id;
+}
+
+
+
+/////
+
+
+
+//Staff::Base(int num_measures) : measures()
+//{
+   //for (unsigned i=0; i<num_measures; i++) measures.push_back(nullptr);
+//}
+
+
+
+Measure::Base *Staff::Base::get_measure(int x_measure)
+{
+   if (x_measure < 0 || x_measure >= get_num_measures()) return nullptr;
+   return measures[x_measure];
+}
+
+
+
+void Staff::Base::set_name(std::string name)
+{
+   this->name = name;
+}
+
+
+
+std::string Staff::Base::get_name()
+{
+   return name;
+}
+
+
+
+int Staff::Base::get_num_measures()
+{
+   return measures.size();
+}
+
+
+
+bool Staff::Base::set_measure(int measure_x, Measure::Base *measure)
+{
+   // TODO move the bounds check to be handled in here
+   // TODO if there is already a measure present, the deletion should be moved to here as well
+   measures[measure_x] = measure;
+   return true;
+}
+
+
+
+bool Staff::Base::insert_measure(int at_index, Measure::Base *measure)
+{
+   // TODO move the bounds check to here
+   // behavior that < 0 inserts are corrected to 0 and
+   // >= size measures default to append() should probably be handled
+   // at the MeasureGrid layer, since it would need to be implement in
+   // all of the derived classes, and is expected behavior of the MeasureGrid
+   measures.insert(measures.begin() + at_index, measure);
+   return true;
+}
+
+
+
+bool Staff::Base::erase_measure(int at_index)
+{
+   // TODO move the bounds check to here
+   // behavior that < 0 inserts are corrected to 0 and
+
+   // >= size measures default to append() should probably be handled
+   // at the MeasureGrid layer, since it would need to be implement in
+   // all of the derived classes, and is expected behavior of the MeasureGrid
+   if (at_index < 0) std::runtime_error("Cannot erase measure < 0");
+   if (at_index >= measures.size()) std::runtime_error("Cannot erase measure >= size()");
+
+   if (measures[at_index]) delete measures[at_index];
+
+   measures.erase(measures.begin() + at_index);
+
+   return true;
+}
+
+
+
+bool Staff::Base::append_measure(Measure::Base *measure)
+{
+   measures.push_back(measure);
+   return true;
 }
 
 
