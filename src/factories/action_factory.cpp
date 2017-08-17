290353fab472cc3375696454828278a1f5296a02
Mark Oates <euph_boy@hotmail.com>
2017-08-17 19:29:52 -0400

    Create ActionFactory scaffolding

---
+++ src/factories/action_factory.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include <fullscore/factories/action_factory.h>
+
+#include <fullscore/app_controller.h>
+
+
+
+Action::Base *ActionFactory::create_action(AppController *app_controller, std::string identifier)
+{
+   return nullptr;
+}
+
+
+
