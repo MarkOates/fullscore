45d223f7c3869b6bee3f010e02647cab3a441973
Mark Oates <euph_boy@hotmail.com>
2017-08-27 14:47:25 -0400

    Add TempoMarking object

---
+++ src/models/tempo_marking.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <fullscore/models/tempo_marking.hpp>
+
+
+
+TempoMarking::TempoMarking(Duration duration, int bpm)
+   : duration(duration)
+   , bpm(bpm)
+{}
+
+
+
+TempoMarking::~TempoMarking()
+{}
+
+
+
