de22eea0413525c8b1a02c8f54295387c8b70d31
Mark Oates <euph_boy@hotmail.com>
2017-10-01 11:32:50 -0400

    Add GridCoordinate (includes staff_id) with tests

---
+++ src/models/grid_coordinate.cpp
@@ -0,0 +1,74 @@
+
+
+
+#include <fullscore/models/grid_coordinate.h>
+
+#include <fullscore/models/grid.h>
+
+
+
+GridCoordinate::GridCoordinate(Grid *grid, int staff_id, int measure_num, int beat_num)
+   : grid(grid)
+   , staff_id(staff_id)
+   , measure_num(measure_num)
+   , beat_num(beat_num)
+{}
+
+
+
+void GridCoordinate::set_grid(Grid *grid)
+{
+   this->grid = grid;
+}
+
+
+
+void GridCoordinate::set_staff_id(int staff_id)
+{
+   this->staff_id = staff_id;
+}
+
+
+
+void GridCoordinate::set_measure_num(int measure_num)
+{
+   this->measure_num = measure_num;
+}
+
+
+
+void GridCoordinate::set_beat_num(int beat_num)
+{
+   this->beat_num = beat_num;
+}
+
+
+
+Grid *GridCoordinate::get_grid()
+{
+   return grid;
+}
+
+
+
+int GridCoordinate::get_staff_id()
+{
+   return staff_id;
+}
+
+
+
+int GridCoordinate::get_measure_num()
+{
+   return measure_num;
+}
+
+
+
+int GridCoordinate::get_beat_num()
+{
+   return beat_num;
+}
+
+
+
---
+++ tests/models/grid_coordinate_test.cpp
@@ -0,0 +1,97 @@
+
+
+
+#include <gtest/gtest.h>
+
+#include <fullscore/models/grid_coordinate.h>
+#include <fullscore/models/grid.h>
+
+
+
+TEST(GridCoordinate, can_be_constructed_with_no_arguments)
+{
+   GridCoordinate grid_coordinate;
+
+   EXPECT_EQ(nullptr, grid_coordinate.get_grid());
+   EXPECT_EQ(0, grid_coordinate.get_staff_id());
+   EXPECT_EQ(0, grid_coordinate.get_measure_num());
+   EXPECT_EQ(0, grid_coordinate.get_beat_num());
+}
+
+
+
+TEST(GridCoordinate, can_be_constructed_with_arguments)
+{
+   Grid grid(1, 1);
+   GridCoordinate grid_coordinate(&grid, 6, 13, 42);
+
+   EXPECT_EQ(&grid, grid_coordinate.get_grid());
+   EXPECT_EQ(6, grid_coordinate.get_staff_id());
+   EXPECT_EQ(13, grid_coordinate.get_measure_num());
+   EXPECT_EQ(42, grid_coordinate.get_beat_num());
+}
+
+
+
+TEST(GridCoordinate, gets_and_sets_the_grid)
+{
+   GridCoordinate grid_coordinate;
+
+   Grid grid(1, 1);
+
+   grid_coordinate.set_grid(&grid);
+   EXPECT_EQ(&grid, grid_coordinate.get_grid());
+}
+
+
+
+TEST(GridCoordinate, gets_and_sets_the_staff_id)
+{
+   GridCoordinate grid_coordinate;
+
+   grid_coordinate.set_staff_id(42);
+   EXPECT_EQ(42, grid_coordinate.get_staff_id());
+}
+
+
+
+TEST(GridCoordinate, gets_and_sets_the_measure_num)
+{
+   GridCoordinate grid_coordinate;
+
+   grid_coordinate.set_measure_num(123);
+   EXPECT_EQ(123, grid_coordinate.get_measure_num());
+
+   grid_coordinate.set_measure_num(-86);
+   EXPECT_EQ(-86, grid_coordinate.get_measure_num());
+
+   grid_coordinate.set_measure_num(720782);
+   EXPECT_EQ(720782, grid_coordinate.get_measure_num());
+}
+
+
+
+TEST(GridCoordinate, gets_and_sets_the_beat_num)
+{
+   GridCoordinate grid_coordinate;
+
+   grid_coordinate.set_beat_num(123);
+   EXPECT_EQ(123, grid_coordinate.get_beat_num());
+
+   grid_coordinate.set_beat_num(-86);
+   EXPECT_EQ(-86, grid_coordinate.get_beat_num());
+
+   grid_coordinate.set_beat_num(720782);
+   EXPECT_EQ(720782, grid_coordinate.get_beat_num());
+}
+
+
+
+int main(int argc, char **argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
+
+
+
