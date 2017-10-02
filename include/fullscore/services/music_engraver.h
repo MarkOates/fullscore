8dbb97591d539b5744b575c566e911a9ea6d8af4
Mark Oates <euph_boy@hotmail.com>
2017-10-01 22:30:02 -0400

    Enable MusicEngraver to color the staff lines and notation

--- include/fullscore/services/music_engraver.h
+++ include/fullscore/services/music_engraver.h
@@ -5,6 +5,7 @@
 
 
 #include <allegro_flare/render_music_notation.h>
+#include <allegro_flare/color.h>
 #include <fullscore/models/Note.h>
 #include <fullscore/models/measures/basic.h>
 
@@ -16,7 +17,7 @@ public:
 	MusicNotation music_notation;
 	MusicEngraver();
 	std::string translate_note_to_str(const Note &note);
-	void draw(Measure::Base *measure, float x, float y, const float whole_note_width);
+	void draw(Measure::Base *measure, float x, float y, const float whole_note_width, ALLEGRO_COLOR notation_color=color::null_color, ALLEGRO_COLOR staff_color=color::null_color);
 };
 
 
--- src/services/music_engraver.cpp
+++ src/services/music_engraver.cpp
@@ -51,13 +51,24 @@ std::string MusicEngraver::translate_note_to_str(const Note &note)
 
 
 
-void MusicEngraver::draw(Measure::Base *measure, float x, float y, const float whole_note_width)
+void MusicEngraver::draw(Measure::Base *measure, float x, float y, const float whole_note_width, ALLEGRO_COLOR notation_color, ALLEGRO_COLOR staff_color)
 {
 	int cursor_x = 0;
    std::vector<Note> notes = measure->get_notes_copy();
 	for (unsigned i=0; i<notes.size(); i++)
 	{
-		music_notation.draw(x + cursor_x, y, translate_note_to_str(notes[i]));
+      std::string notes_as_string = translate_note_to_str(notes[i]);
+      if (!color::basically_equal(notation_color, color::null_color))
+      {
+         std::string color_as_name = al_color_rgb_to_name(notation_color.r, notation_color.g, notation_color.b);
+         notes_as_string = std::string("{color=") + color_as_name + "}" + notes_as_string;
+      }
+      if (!color::basically_equal(staff_color, color::null_color))
+      {
+         std::string color_as_name = al_color_rgb_to_name(staff_color.r, staff_color.g, staff_color.b);
+         notes_as_string = std::string("{staff_color=") + color_as_name + "}" + notes_as_string;
+      }
+		music_notation.draw(x + cursor_x, y, notes_as_string);
 		cursor_x += DurationHelper::get_length(notes[i].duration.denominator, notes[i].duration.dots) * whole_note_width;
 	}
 }
