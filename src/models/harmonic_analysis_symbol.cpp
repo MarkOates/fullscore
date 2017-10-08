a56f7a167a83373ab33659e5ae175360805d269b
Mark Oates <euph_boy@hotmail.com>
2017-10-07 22:51:56 -0400

    Add get_roman_numeral_string to HarmonicAnalysisSymbol (w/tests)

--- src/models/harmonic_analysis_symbol.cpp
+++ src/models/harmonic_analysis_symbol.cpp
@@ -34,3 +34,25 @@ std::string HarmonicAnalysisSymbol::get_chord_quality_string(chord_quality_t cho
 
 
 
+std::string HarmonicAnalysisSymbol::get_roman_numeral_string(int number, bool quality_is_major)
+{
+   switch(number)
+   {
+   case 0: return quality_is_major ? "O" : "o"; break;
+   case 1: return quality_is_major ? "I" : "i"; break;
+   case 2: return quality_is_major ? "II" : "ii"; break;
+   case 3: return quality_is_major ? "III" : "iii"; break;
+   case 4: return quality_is_major ? "IV" : "iv"; break;
+   case 5: return quality_is_major ? "V" : "v"; break;
+   case 6: return quality_is_major ? "VI" : "vi"; break;
+   case 7: return quality_is_major ? "VII" : "vii"; break;
+   case 8: return quality_is_major ? "IIX" : "iix"; break;
+   case 9: return quality_is_major ? "IX" : "ix"; break;
+   case 10: return quality_is_major ? "X" : "x"; break;
+   case 11: return quality_is_major ? "XI" : "xi"; break;
+   default: return "--"; break;
+   }
+}
+
+
+
---
+++ tests/models/harmonic_analysis_symbol_test.cpp
@@ -0,0 +1,53 @@
+
+
+
+#include <gtest/gtest.h>
+
+#include <fullscore/models/harmonic_analysis_symbol.h>
+
+
+
+TEST(HarmonicAnalysisSymbolTest, can_be_created)
+{
+   HarmonicAnalysisSymbol harmonic_analysis_symbol(0, HarmonicAnalysisSymbol::MAJOR, 0, {});
+}
+
+
+
+TEST(HarmonicAnalysisSymbolTest, returns_major_and_minor_roman_numerals_for_values_0_to_11)
+{
+   std::vector<std::string> expected_major_values = { "O", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX", "X", "XI" };
+   std::vector<std::string> expected_minor_values = { "o", "i", "ii", "iii", "iv", "v", "vi", "vii", "iix", "ix", "x", "xi" };
+
+   for (int i=0; i<expected_major_values.size(); i++)
+   {
+      ASSERT_EQ(expected_major_values[i], HarmonicAnalysisSymbol::get_roman_numeral_string(i, true));
+      ASSERT_EQ(expected_minor_values[i], HarmonicAnalysisSymbol::get_roman_numeral_string(i, false));
+   }
+}
+
+
+
+TEST(HarmonicAnalysisSymbolTest, with_a_scale_degree_that_does_not_exist_returns_dashes)
+{
+   std::string expected_string = "--";
+
+   ASSERT_EQ(expected_string, HarmonicAnalysisSymbol::get_roman_numeral_string(12, true));
+   ASSERT_EQ(expected_string, HarmonicAnalysisSymbol::get_roman_numeral_string(999, true));
+   ASSERT_EQ(expected_string, HarmonicAnalysisSymbol::get_roman_numeral_string(-1, true));
+
+   ASSERT_EQ(expected_string, HarmonicAnalysisSymbol::get_roman_numeral_string(12, false));
+   ASSERT_EQ(expected_string, HarmonicAnalysisSymbol::get_roman_numeral_string(999, false));
+   ASSERT_EQ(expected_string, HarmonicAnalysisSymbol::get_roman_numeral_string(-1, false));
+}
+
+
+
+int main(int argc, char **argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
+
+
+
