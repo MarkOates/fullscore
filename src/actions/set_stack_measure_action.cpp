0f79d2510f14f01aa3e8e514b8d2ba77bb81e33e
Mark Oates <euph_boy@hotmail.com>
2017-10-29 12:31:57 -0400

    Remove Transform::Copy

--- src/actions/set_stack_measure_action.cpp
+++ src/actions/set_stack_measure_action.cpp
@@ -6,12 +6,6 @@
 #include <fullscore/models/measures/stack.h>
 #include <fullscore/models/grid.h>
 
-// this next few lines are for testing
-#include <fullscore/transforms/copy_transform.h>
-#include <fullscore/transforms/double_duration_transform.h>
-#include <fullscore/transforms/retrograde_transform.h>
-#include <fullscore/transforms/transpose_transform.h>
-
 
 
 Action::SetStackMeasure::SetStackMeasure(Grid *grid, int measure_x, int staff_y)
@@ -35,13 +29,6 @@ bool Action::SetStackMeasure::execute()
    Measure::Stack *new_stack_measure = new Measure::Stack();
 
    bool measure_set_successfully = grid->set_measure(measure_x, staff_y, new_stack_measure);
-
-   // these next transform lines are simply for testing
-   Measure::Stack *measure =  static_cast<Measure::Stack *>(grid->get_measure(measure_x, staff_y));
-   measure->transformations.add_transform(new Transform::Copy(grid, 0, 0));
-   measure->transformations.add_transform(new Transform::DoubleDuration());
-   measure->transformations.add_transform(new Transform::Retrograde());
-   measure->transformations.add_transform(new Transform::Transpose(-5));
 
    if (!measure_set_successfully)
    {
