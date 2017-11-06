0dc09230cc1f4cc4d05fa536179335dfe6e29100
Mark Oates <euph_boy@hotmail.com>
2017-11-05 23:23:27 -0500

    Add InstrumentFactory; Include create_trombone()

---
+++ include/fullscore/factories/instrument_factory.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+
+#include <fullscore/models/staves/instrument.h>
+
+
+
+class InstrumentFactory
+{
+public:
+   Staff::Instrument *create_trombone();
+};
+
+
+
---
+++ src/factories/instrument_factory.cpp
@@ -0,0 +1,33 @@
+
+
+
+#include <fullscore/factories/instrument_factory.h>
+
+#include <fullscore/converters/scientific_pitch_notation_to_pitch_converter.h>
+#include <fullscore/instrument_attributes.h>
+
+
+
+Staff::Instrument *InstrumentFactory::create_trombone()
+{
+   Staff::Instrument *instrument = new Staff::Instrument("Trombone");
+
+   // lineage
+   instrument->attributes.set(InstrumentAttribute::FAMILY, InstrumentAttribute::Family::BRASS);
+
+   // roles
+   instrument->attributes.set(InstrumentAttribute::VOICE_ROLE, InstrumentAttribute::VoiceRole::TENOR);
+
+   // range
+   instrument->attributes.set(InstrumentAttribute::COMFORTABLE_RANGE_MIN, SPNToPitchConverter('F', 2).convert());
+   instrument->attributes.set(InstrumentAttribute::COMFORTABLE_RANGE_MAX, SPNToPitchConverter('F', 4).convert());
+   instrument->attributes.set(InstrumentAttribute::EXTENDED_RANGE_MIN, SPNToPitchConverter('G', 1).convert());
+   instrument->attributes.set(InstrumentAttribute::EXTENDED_RANGE_MAX, SPNToPitchConverter('B', 4, -1).convert());
+   instrument->attributes.set(InstrumentAttribute::HYPER_EXTENDED_RANGE_MIN, SPNToPitchConverter('F', 1).convert());
+   instrument->attributes.set(InstrumentAttribute::HYPER_EXTENDED_RANGE_MAX, SPNToPitchConverter('D', 5).convert());
+
+   return instrument;
+}
+
+
+
