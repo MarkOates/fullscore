d5d5267b88b2ca472a5906d20c4ca29786a4972e
Mark Oates <euph_boy@hotmail.com>
2017-12-03 20:54:39 -0500

    Do not throw error when resetting floating_measure

--- src/actions/reset_floating_measure_cursor_action.cpp
+++ src/actions/reset_floating_measure_cursor_action.cpp
@@ -28,19 +28,9 @@ bool Action::ResetFloatingMeasureCursor::execute()
    if (!floating_measure_cursor) throw std::runtime_error("Cannot reset nullptr floating_measure_cursor");
 
    FloatingMeasure *found_floating_measure = FloatingMeasure::find_first_in_staff_after_barline(staff_id, barline_num);
-   if (!found_floating_measure)
-   {
-      std::stringstream error_message;
-      error_message << "Cannot set floating measure cursor because there is not floating measure at (or beyond) the position (staff_id: "
-         << staff_id
-         << ", barline_num: "
-         << barline_num
-         << ")"
-         << std::endl;
-      throw std::runtime_error(error_message.str());
-   }
 
-   floating_measure_cursor->set_floating_measure_id(found_floating_measure->get_id());
+   if (!found_floating_measure) floating_measure_cursor->clear_floating_measure_id();
+   else floating_measure_cursor->set_floating_measure_id(found_floating_measure->get_id());
 
    return true;
 }
