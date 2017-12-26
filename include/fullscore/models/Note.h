2b12d18672e761c50b9e6779ea8b55e1a8e3edd5
Mark Oates <euph_boy@hotmail.com>
2017-12-26 15:06:20 -0500

    Add getters and setters to Note

--- include/fullscore/models/Note.h
+++ include/fullscore/models/Note.h
@@ -10,13 +10,22 @@
 
 class Note
 {
-public:
+private:
    Pitch pitch;
    Duration duration;
    int is_rest;
 
+public:
    Note(int _scale_degree=0, Duration duration=Duration());
    ~Note();
+
+   bool set_pitch(Pitch pitch);
+   bool set_duration(Duration duration);
+   bool set_rest(bool rest);
+
+   Pitch get_pitch();
+   Duration get_duration();
+   bool get_rest();
 
    bool operator==(const Note &other) const;
 };
--- src/models/note.cpp
+++ src/models/note.cpp
@@ -18,6 +18,51 @@ Note::~Note()
 
 
 
+bool Note::set_pitch(Pitch pitch)
+{
+   this->pitch = pitch;
+   return true;
+}
+
+
+
+bool Note::set_duration(Duration duration)
+{
+   this->duration = duration;
+   return true;
+}
+
+
+
+bool Note::set_rest(bool rest)
+{
+   this->is_rest = rest;
+   return true;
+}
+
+
+
+Pitch Note::get_pitch()
+{
+   return pitch;
+}
+
+
+
+Duration Note::get_duration()
+{
+   return duration;
+}
+
+
+
+bool Note::get_rest()
+{
+   return is_rest;
+}
+
+
+
 bool Note::operator==(const Note &other) const
 {
    return (pitch == other.pitch
