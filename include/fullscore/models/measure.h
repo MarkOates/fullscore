0fe89cda1ea5586dd30b8cf8f804ad79822406cd
Mark Oates <euph_boy@hotmail.com>
2017-12-25 21:22:44 -0500

    Style: Fix excessive spaces in list of definitions

--- include/fullscore/models/measure.h
+++ include/fullscore/models/measure.h
@@ -10,12 +10,12 @@
 
 namespace Measure
 {
-   std::string const TYPE_IDENTIFIER_BASE                    = "base";
-   std::string const TYPE_IDENTIFIER_BASIC                   = "basic";
-   std::string const TYPE_IDENTIFIER_PLOTTED                 = "plotted";
-   std::string const TYPE_IDENTIFIER_REFERENCE_BY_ID         = "reference_by_id";
-   std::string const TYPE_IDENTIFIER_STACK                   = "stack";
-   std::string const TYPE_IDENTIFIER_STATIC                  = "static";
+   std::string const TYPE_IDENTIFIER_BASE            = "base";
+   std::string const TYPE_IDENTIFIER_BASIC           = "basic";
+   std::string const TYPE_IDENTIFIER_PLOTTED         = "plotted";
+   std::string const TYPE_IDENTIFIER_REFERENCE_BY_ID = "reference_by_id";
+   std::string const TYPE_IDENTIFIER_STACK           = "stack";
+   std::string const TYPE_IDENTIFIER_STATIC          = "static";
 
    enum find_option_t
    {
