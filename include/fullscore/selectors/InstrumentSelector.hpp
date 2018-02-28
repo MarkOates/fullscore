ae0b542efc5584a41a93a8e972aa218ea8bdfd46
Mark Oates <euph_boy@hotmail.com>
2018-02-27 20:05:14 -0500

    Add more advanced selectors to InstrumentSelector

--- include/fullscore/selectors/InstrumentSelector.hpp
+++ include/fullscore/selectors/InstrumentSelector.hpp
@@ -14,11 +14,21 @@ public:
    InstrumentSelector(std::vector<Staff::Base *> &pool=Staff::pool);
    ~InstrumentSelector();
 
+   std::vector<Staff::Instrument *> instruments();
    std::vector<Staff::Instrument *> matches(std::string attribute, std::string property);
+
    std::vector<Staff::Instrument *> soprano_voices();
    std::vector<Staff::Instrument *> alto_voices();
    std::vector<Staff::Instrument *> tenor_voices();
+   std::vector<Staff::Instrument *> baritone_voices();
    std::vector<Staff::Instrument *> bass_voices();
+
+   std::vector<Staff::Instrument *> woodwinds();
+   std::vector<Staff::Instrument *> brass();
+   std::vector<Staff::Instrument *> strings();
+
+   std::vector<Staff::Instrument *> join(std::vector<std::vector<Staff::Instrument *>> instruments);
+
    std::vector<int> ids(std::vector<Staff::Instrument *> &instruments);
 };
 
--- src/selectors/InstrumentSelector.cpp
+++ src/selectors/InstrumentSelector.cpp
@@ -16,6 +16,18 @@ InstrumentSelector::~InstrumentSelector()
 }
 
 
+std::vector<Staff::Instrument *> InstrumentSelector::instruments()
+{
+   std::vector<Staff::Instrument *> results;
+
+   for (auto &element : pool)
+      if (element->is_type(Staff::TYPE_IDENTIFIER_INSTRUMENT))
+         results.push_back(static_cast<Staff::Instrument *>(element));
+
+   return results;
+}
+
+
 std::vector<Staff::Instrument *> InstrumentSelector::matches(std::string attribute, std::string property)
 {
    std::vector<Staff::Instrument *> results;
@@ -48,9 +60,46 @@ std::vector<Staff::Instrument *> InstrumentSelector::tenor_voices()
 }
 
 
+std::vector<Staff::Instrument *> InstrumentSelector::baritone_voices()
+{
+   return matches(InstrumentAttribute::VOICE_ROLE, InstrumentAttribute::VoiceRole::BARITONE);
+}
+
+
 std::vector<Staff::Instrument *> InstrumentSelector::bass_voices()
 {
    return matches(InstrumentAttribute::VOICE_ROLE, InstrumentAttribute::VoiceRole::BASS);
+}
+
+
+std::vector<Staff::Instrument *> InstrumentSelector::woodwinds()
+{
+   return matches(InstrumentAttribute::FAMILY, InstrumentAttribute::Family::WOODWIND);
+}
+
+
+std::vector<Staff::Instrument *> InstrumentSelector::brass()
+{
+   return matches(InstrumentAttribute::FAMILY, InstrumentAttribute::Family::BRASS);
+}
+
+
+std::vector<Staff::Instrument *> InstrumentSelector::strings()
+{
+   return matches(InstrumentAttribute::FAMILY, InstrumentAttribute::Family::STRING);
+}
+
+
+std::vector<Staff::Instrument *> InstrumentSelector::join(std::vector<std::vector<Staff::Instrument *>> instruments)
+{
+   std::vector<Staff::Instrument *> results;
+   for (auto &instrs : instruments)
+      for (auto &instrument : instrs)
+      {
+         results.push_back(instrument);
+      }
+
+   return std::vector<Staff::Instrument *>(results);
 }
 
 
