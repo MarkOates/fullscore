fad7cae9c377a5447d49bbdb09815364f2976801
Mark Oates <euph_boy@hotmail.com>
2018-02-28 00:26:58 -0500

    Expand "development" score with more plotting examples

--- src/factories/GridFactory.cpp
+++ src/factories/GridFactory.cpp
@@ -13,7 +13,10 @@
 #include <fullscore/models/staves/Tempo.h>
 #include <fullscore/models/Note.h>
 #include <fullscore/selectors/InstrumentSelector.hpp>
+#include <fullscore/transforms/HalfDurationTransform.h>
+#include <fullscore/transforms/Octatonic1Transform.h>
 #include <fullscore/transforms/RetrogradeTransform.h>
+#include <fullscore/transforms/TransposeTransform.h>
 #include <fullscore/InstrumentAttributes.h>
 #include <allegro_flare/useful.h>
 #include <iostream>
@@ -24,6 +27,15 @@ std::string const SPACER = "-";
 std::string const MEASURE_NUMBERS = "#";
 std::string const TEMPO = "!";
 std::string const HARMONIC_ANALYSIS = "V";
+
+
+
+static int __average_pitch(std::vector<Note> &notes)
+{
+   int sum = 0;
+   for (auto &note : notes) sum = note.get_pitch().get_scale_degree() + note.get_pitch().get_accidental();
+   return sum / notes.size();
+}
 
 
 
@@ -202,7 +214,7 @@ Grid GridFactory::development()
    FloatingMeasure *floating_measure = new FloatingMeasure(GridCoordinate(&current_grid_editor->grid, staff_to_put_measure->get_id(), 2), basic_measure->get_id());
 */
 
-   Grid grid(12);
+   Grid grid(12*4);
 
 
    InstrumentFactory instrument_factory;
@@ -211,14 +223,22 @@ Grid GridFactory::development()
    grid.append_staff(new Staff::MeasureNumbers);
    grid.append_staff(new Staff::Tempo);
    grid.append_staff(instrument_factory.create_flute());
-   grid.append_staff(instrument_factory.create_clarinet());
    grid.append_staff(instrument_factory.create_oboe());
+   grid.append_staff(instrument_factory.create_clarinet());
    grid.append_staff(instrument_factory.create_bassoon());
    grid.append_staff(new Staff::Spacer());
    grid.append_staff(instrument_factory.create_trumpet());
+   grid.append_staff(instrument_factory.create_trumpet());
    grid.append_staff(instrument_factory.create_french_horn());
    grid.append_staff(instrument_factory.create_trombone());
+   grid.append_staff(instrument_factory.create_bass_trombone());
    grid.append_staff(instrument_factory.create_tuba());
+   grid.append_staff(new Staff::Spacer());
+   grid.append_staff(instrument_factory.create_violin());
+   grid.append_staff(instrument_factory.create_violin());
+   grid.append_staff(instrument_factory.create_viola());
+   grid.append_staff(instrument_factory.create_cello());
+   grid.append_staff(instrument_factory.create_bass());
 
    Note rest_note = Note(0, Duration(Duration::QUARTER));
    rest_note.set_rest(true);
@@ -231,6 +251,12 @@ Grid GridFactory::development()
       rest_note,
    };
 
+   std::vector<Note> octanotes = Transform::Retrograde().transform(Transform::HalfDuration().transform(Transform::Octatonic1().transform({})));
+   std::vector<Note> octanotes_1 = Transform::Transpose(12).transform(octanotes);
+   std::vector<Note> octanotes_2 = Transform::Transpose(24).transform(octanotes);
+   std::vector<Note> octanotes_n1 = Transform::Transpose(-12).transform(octanotes);
+   std::vector<Note> octanotes_n2 = Transform::Transpose(-24).transform(octanotes);
+
    auto notes_retrograde = Transform::Retrograde().transform(notes_to_plot);
 
 
@@ -239,37 +265,153 @@ Grid GridFactory::development()
    std::vector<Staff::Instrument *> soprano_instruments = instrument_selector.soprano_voices();
    std::vector<Staff::Instrument *> alto_instruments = instrument_selector.alto_voices();
    std::vector<Staff::Instrument *> tenor_instruments = instrument_selector.tenor_voices();
+   std::vector<Staff::Instrument *> baritone_instruments = instrument_selector.baritone_voices();
    std::vector<Staff::Instrument *> bass_instruments = instrument_selector.bass_voices();
+   std::vector<Staff::Instrument *> tenor_and_baritone_instruments = instrument_selector.join({ tenor_instruments, baritone_instruments });
+
+   std::vector<Staff::Instrument *> woodwind_instruments = instrument_selector.woodwinds();
+   std::vector<Staff::Instrument *> brass_instruments = instrument_selector.brass();
+   std::vector<Staff::Instrument *> string_instruments = instrument_selector.strings();
+
+   std::vector<Staff::Instrument *> all_instruments = instrument_selector.instruments();
 
    std::vector<int> soprano_staff_ids = instrument_selector.ids(soprano_instruments);
    std::vector<int> alto_staff_ids = instrument_selector.ids(alto_instruments);
-   std::vector<int> tenor_staff_ids = instrument_selector.ids(tenor_instruments);
+
+   std::vector<int> tenor_and_baritone_staff_ids = instrument_selector.ids(tenor_and_baritone_instruments);
    std::vector<int> bass_staff_ids = instrument_selector.ids(bass_instruments);
 
+   std::vector<int> woodwind_staff_ids = instrument_selector.ids(woodwind_instruments);
+   std::vector<int> brass_staff_ids = instrument_selector.ids(brass_instruments);
+   std::vector<int> string_staff_ids = instrument_selector.ids(string_instruments);
+   std::vector<int> all_instrument_staff_ids = instrument_selector.ids(all_instruments);
 
-   for (unsigned barline_num=0; barline_num<3; barline_num++)
+   int measure_cursor = 0;
+   int interval = 2;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
    {
       Plotter::Staff staff_plotter = Plotter::Staff(soprano_staff_ids, barline_num, notes_to_plot);
       staff_plotter.plot();
    }
 
-   for (unsigned barline_num=3; barline_num<6; barline_num++)
+   measure_cursor += interval;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
    {
       Plotter::Staff staff_plotter = Plotter::Staff(alto_staff_ids, barline_num, notes_retrograde);
       staff_plotter.plot();
    }
 
-   for (unsigned barline_num=6; barline_num<9; barline_num++)
+   measure_cursor += interval;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
    {
-      Plotter::Staff staff_plotter = Plotter::Staff(tenor_staff_ids, barline_num, notes_to_plot);
+      Plotter::Staff staff_plotter = Plotter::Staff(tenor_and_baritone_staff_ids, barline_num, notes_to_plot);
       staff_plotter.plot();
    }
 
-   for (unsigned barline_num=9; barline_num<grid.get_num_barlines(); barline_num++)
+   measure_cursor += interval;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
    {
-      Plotter::Staff staff_plotter = Plotter::Staff(bass_staff_ids, barline_num, notes_retrograde);
+      Plotter::Staff staff_plotter = Plotter::Staff(bass_staff_ids, barline_num, octanotes);
       staff_plotter.plot();
    }
+
+   measure_cursor += interval;
+
+   // by family
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      Plotter::Staff staff_plotter = Plotter::Staff(woodwind_staff_ids, barline_num, octanotes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      Plotter::Staff staff_plotter = Plotter::Staff(brass_staff_ids, barline_num, octanotes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      Plotter::Staff staff_plotter = Plotter::Staff(string_staff_ids, barline_num, octanotes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      Plotter::Staff staff_plotter = Plotter::Staff(all_instrument_staff_ids, barline_num, octanotes);
+      staff_plotter.plot();
+   }
+
+   std::vector<Staff::Instrument *> instruments_in_range;
+   std::vector<Note> *notes;
+
+   measure_cursor += interval;
+   notes = &octanotes_2;
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      int average_pitch = __average_pitch(*notes);
+      instruments_in_range = instrument_selector.within_basic_range(average_pitch);
+      std::vector<int> staff_ids = instrument_selector.ids(instruments_in_range);
+      Plotter::Staff staff_plotter = Plotter::Staff(staff_ids, barline_num, *notes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+   notes = &octanotes_1;
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      int average_pitch = __average_pitch(*notes);
+      instruments_in_range = instrument_selector.within_basic_range(average_pitch);
+      std::vector<int> staff_ids = instrument_selector.ids(instruments_in_range);
+      Plotter::Staff staff_plotter = Plotter::Staff(staff_ids, barline_num, *notes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+   notes = &octanotes;
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      int average_pitch = __average_pitch(*notes);
+      instruments_in_range = instrument_selector.within_basic_range(average_pitch);
+      std::vector<int> staff_ids = instrument_selector.ids(instruments_in_range);
+      Plotter::Staff staff_plotter = Plotter::Staff(staff_ids, barline_num, *notes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+   notes = &octanotes_n1;
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      int average_pitch = __average_pitch(*notes);
+      instruments_in_range = instrument_selector.within_basic_range(average_pitch);
+      std::vector<int> staff_ids = instrument_selector.ids(instruments_in_range);
+      Plotter::Staff staff_plotter = Plotter::Staff(staff_ids, barline_num, *notes);
+      staff_plotter.plot();
+   }
+
+   measure_cursor += interval;
+   notes = &octanotes_n2;
+   for (unsigned barline_num=measure_cursor; barline_num<measure_cursor+interval; barline_num++)
+   {
+      int average_pitch = __average_pitch(*notes);
+      instruments_in_range = instrument_selector.within_basic_range(average_pitch);
+      std::vector<int> staff_ids = instrument_selector.ids(instruments_in_range);
+      Plotter::Staff staff_plotter = Plotter::Staff(staff_ids, barline_num, *notes);
+      staff_plotter.plot();
+   }
+
+
 
 
    return grid;
