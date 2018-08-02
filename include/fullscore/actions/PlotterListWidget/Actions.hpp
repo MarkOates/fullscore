2285b92623d27853692b15b09f5f1868aa8b62b3
Mark Oates <euph_boy@hotmail.com>
2018-08-02 07:34:45 -0400

    Use C++17 nested namespace notation in Actions::PlotterListWidget

--- include/fullscore/actions/PlotterListWidget/Actions.hpp
+++ include/fullscore/actions/PlotterListWidget/Actions.hpp
@@ -1,14 +1,11 @@
 #pragma once
 
 
-namespace Actions
-{
-namespace PlotterListWidget
+namespace Actions::PlotterListWidget
 {
    std::string const MOVE_CURSOR_UP_ACTION_IDENTIFIER = "move_cursor_up";
    std::string const MOVE_CURSOR_DOWN_ACTION_IDENTIFIER = "move_cursor_down";
    std::string const SELECT_PLOTTER_ACTION_IDENTIFIER = "select_plotter";
-}
 }
 
 
