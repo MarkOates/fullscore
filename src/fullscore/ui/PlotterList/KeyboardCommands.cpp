0ed58cbce83fac90ce63ee829b8c647b84037696
Mark Oates <euph_boy@hotmail.com>
2018-08-04 19:03:34 -0400

    Fix PlotterList::KeyboardCommands K and J mappings

--- src/fullscore/ui/PlotterList/KeyboardCommands.cpp
+++ src/fullscore/ui/PlotterList/KeyboardCommands.cpp
@@ -21,8 +21,8 @@ KeyboardCommands::~KeyboardCommands()
 
 void KeyboardCommands::set_keyboard_commands()
 {
-   set_mapping(ALLEGRO_KEY_J,     false, false, false, {Actions::MOVE_CURSOR_UP_ACTION_IDENTIFIER});
-   set_mapping(ALLEGRO_KEY_K,     false, false, false, {Actions::MOVE_CURSOR_DOWN_ACTION_IDENTIFIER});
+   set_mapping(ALLEGRO_KEY_K,     false, false, false, {Actions::MOVE_CURSOR_UP_ACTION_IDENTIFIER});
+   set_mapping(ALLEGRO_KEY_J,     false, false, false, {Actions::MOVE_CURSOR_DOWN_ACTION_IDENTIFIER});
    set_mapping(ALLEGRO_KEY_ENTER, false, false, false, {Actions::SELECT_PLOTTER_ACTION_IDENTIFIER});
 }
 
