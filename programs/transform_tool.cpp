6ddf0b6fa91ae61e6f74f95a982e23df8f2850c7
Mark Oates <markoates0@gmail.com>
2019-03-10 23:20:28 -0400

    Expand add-transform-tool with NoteSetRenderer

--- programs/transform_tool.cpp
+++ programs/transform_tool.cpp
@@ -9,6 +9,33 @@
 #include <fullscore/transforms/AddDotTransform.h>
 
 
+#include <allegro_flare/render_cache.h>
+
+
+
+class NoteSetRenderer
+{
+private:
+   NoteSet &note_set;
+   std::string output_filename;
+
+public:
+   NoteSetRenderer(NoteSet &note_set, std::string output_filename)
+      : note_set(note_set)
+      , output_filename(output_filename)
+   {}
+
+   void render()
+   {
+      RenderCache render_cache;
+      //render_cache
+      //setup_surface(int w, int h, int multisamples=0, int depth=0);
+      //finish_surface();
+      al_save_bitmap(output_filename.c_str(), render_cache.get_surface_bitmap());
+   }
+};
+
+
 
 int main(int argv, char **argc)
 {
@@ -20,15 +47,8 @@ int main(int argv, char **argc)
    Measure::Basic measure_before_transform(note_set.get_notes());
    Measure::Basic measure_after_transform(result_notes_set.get_notes());
 
-   std::cout << "before transform:" << std::endl;
-   for (auto &note : measure_before_transform.get_notes_copy())
-      std::cout << note << std::endl;
-
-   std::cout << "after transform:" << std::endl;
-   for (auto &note : measure_after_transform.get_notes_copy())
-      std::cout << note << std::endl;
-
-   return 0;
+   NoteSetRenderer(note_set, "notes-before.png");
+   NoteSetRenderer(result_notes_set, "notes-after.png");
 }
 
 
