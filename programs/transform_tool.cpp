71963e2cdf0d435cc781775f2fed0a1ce010d915
Mark Oates <markoates0@gmail.com>
2019-03-10 22:33:50 -0400

    Add basic result set calculation

--- programs/transform_tool.cpp
+++ programs/transform_tool.cpp
@@ -12,8 +12,12 @@
 int main(int argv, char **argc)
 {
    NoteSet note_set({ { 0 }, { 1 } });
+   NoteSet result_notes_set{{}};
+
+   result_notes_set = Transform::AddDot().transform(note_set.get_notes());
 
    Measure::Basic measure_before_transform(note_set.get_notes());
+   Measure::Basic measure_after_transform(result_notes_set.get_notes());
 
    return 0;
 }
