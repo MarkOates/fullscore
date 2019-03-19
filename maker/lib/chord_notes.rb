81567ada5522e960802fe62ce59420f4bc5cb92c
Mark Oates <markoates0@gmail.com>
2019-03-18 23:12:16 -0400

    Add Chromatisizer

--- maker/lib/chord_notes.rb
+++ maker/lib/chord_notes.rb
@@ -1,3 +1,42 @@
+class Chromatisizer
+  attr_reader :notes
+
+  def initialize(notes)
+    @notes = notes
+  end
+
+  def chromatics
+    notes.map { |note| chromatic(note) }
+  end
+
+  private
+
+  def chromatic(diatonic)
+    case diatonic
+    when 1
+      0
+    when 2
+      2
+    when 3
+      4
+    when 4
+      5
+    when 5
+      7
+    when 6
+      9
+    when 7
+      11
+    when 8
+      13
+    when 9
+      15
+    else
+      throw StandardError.new("unknown diatonic value")
+    end
+  end
+end
+
 class ChordNotes
   def self.notes_for(progression)
     chords = ChordNotes.new.chords(progression)
--- maker/tests/chord_notes_test.rb
+++ maker/tests/chord_notes_test.rb
@@ -2,6 +2,14 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require_relative '../lib/chord_notes'
 
+class ChromatisizerTest < Minitest::Test
+  def test_converts_diatonic_note_to_chromatic
+    source_notes = [1, 3, 5]
+    expected_notes = [0, 4, 7]
+    assert_equal expected_notes, Chromatisizer.new(source_notes).chromatics
+  end
+end
+
 class ChordNotesTest < Minitest::Test
   def test_plots_notes_in_a_chord
     expcected = [1, 3, 5]
