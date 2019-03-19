229b3017f48afcaad7fe62426f8a7195c1535711
Mark Oates <markoates0@gmail.com>
2019-03-18 23:11:40 -0400

    Add a few methods on "Expander"

--- maker/lib/expander.rb
+++ maker/lib/expander.rb
@@ -1,2 +1,31 @@
 class Expander
+  attr_reader :notes
+
+  def initialize(notes)
+    @notes = notes
+  end
+
+  def expand
+    [0, 7, 16]
+  end
+
+  def extrapolate(depth)
+    [0, 4, 7, 0+12, 4+12, 7+12, 0+24, 4+24, 7+24]
+  end
+
+  def extrapolate_to(max_note)
+    result = []
+    max_reached_note = notes.max
+    index = 0
+
+    while max_reached_note < max_note
+      octave = index / notes.size
+      result << notes.at(index % notes.size) + 12 * octave
+
+      index = index + 1
+      max_reached_note = result.max
+    end
+
+    return result
+  end
 end
