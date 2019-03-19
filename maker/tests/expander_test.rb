f19f03088a457d883d066e25b403ac2f2fe9d296
Mark Oates <markoates0@gmail.com>
2019-03-18 22:31:46 -0400

    Add simple Expander

---
+++ maker/tests/expander_test.rb
@@ -0,0 +1,8 @@
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require_relative '../lib/expander'
+
+class ExpanderTest < Minitest::Test
+  def test_converts_diatonic_note_to_chromatic
+  end
+end
