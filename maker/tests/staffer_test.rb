90f11a0aacb05fe75a7c20ae483816a16100be4d
Mark Oates <markoates0@gmail.com>
2019-04-15 23:52:27 -0400

    Extend Staffer test notes

--- maker/tests/staffer_test.rb
+++ maker/tests/staffer_test.rb
@@ -4,14 +4,14 @@ require_relative '../lib/staffer'
 
 class StafferTest < Minitest::Test
   def test_staffer_places_the_notes_into_staves
-    notes = [0, 4, 7]
+    notes = [0, 4, 7, 9, 10, 6 ]
     staffer = Staffer.new(notes: notes)
     expected_output = {
       staff_1: {
-        notes: [0, 7],
+        notes: [0, 7, 10],
       },
       staff_2: {
-        notes: [4],
+        notes: [4, 9, 6],
       },
     }
 
