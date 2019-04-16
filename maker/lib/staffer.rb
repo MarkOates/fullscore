9349e4a1d72b363dd4b6c4f21e0f3f5c0b932e10
Mark Oates <markoates0@gmail.com>
2019-04-15 21:07:02 -0400

    Dynamically distribute notes into staves with Staffer

--- maker/lib/staffer.rb
+++ maker/lib/staffer.rb
@@ -1,4 +1,5 @@
 class Staffer
+  STAVES_COUNT = 2
   attr_reader :notes
 
   def initialize(notes:)
@@ -6,13 +7,14 @@ class Staffer
   end
 
   def staff
-    expected_output = {
-      staff_1: {
-        notes: [0, 7],
-      },
-      staff_2: {
-        notes: [4],
-      },
-    }
+    expected_output = {}
+
+    notes.each_with_index do |note, index|
+      staff_num = index % STAVES_COUNT
+      staff_key_sym = "staff_#{staff_num+1}".to_sym
+      expected_output[staff_key_sym] = { notes: [0, 7] }
+    end
+
+    expected_output
   end
 end
