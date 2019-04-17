e018905958a391f6685e3c542a5ed39695aef807
Mark Oates <markoates0@gmail.com>
2019-04-17 01:06:39 -0400

    Make MelodyExtractor (untested)

--- maker/lib/chromatic/composer.rb
+++ maker/lib/chromatic/composer.rb
@@ -1,22 +1,32 @@
 require_relative 'chord_notes'
+require_relative 'melody_extractor'
 require_relative 'voicer'
 
 module Chromatic
   class Composer
     def composition
       projections = ChordNotes.notes_for('circle_of_5ths')
+      projections << ChordNotes.new.chord_notes('I')
+
+
+      melody = MelodyExtractor.random(projections: projections)
+      melody.pop
+      melody << 12  # make sure it ends on tonic
 
       result = {
         staves: [
           {
-            notes: projections.map { |projection| projection.last },
+            notes: melody,
+          },
+          {
+            notes: projections.map { |projection| projection.last + 12 },
           },
           {
             notes: projections.map { |projection| Voicer.new(projection).open },
           },
           {
             notes: projections.map { |projection| projection.first },
-          }
+          },
         ]
       }
     end
