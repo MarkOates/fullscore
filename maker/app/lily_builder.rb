296bb93313a0b983511257884b1e4444ac04ef08
Mark Oates <markoates0@gmail.com>
2019-04-16 23:48:02 -0400

    lily_builder splits output into multiple staves

--- maker/app/lily_builder.rb
+++ maker/app/lily_builder.rb
@@ -1,34 +1,28 @@
 TEMPLATE_FILE = 'templates/lilypond-template.ly'
 OUTPUT_FILE = 'output-lily.ly'
 
-require_relative '../lib/lily_converter'
-require_relative '../lib/voicer'
+require_relative '../lib/chromatic/lily_converter'
+require_relative '../lib/template_stuffer'
+require_relative '../lib/chromatic/voicer'
+require_relative '../lib/chromatic/composer'
 
 
-# todo, ellaborate this class
-class NoteBuilder
-  def ly_notes
-    notes = [0, [2, 4, 5], 8, 11, 4]
-    #notes = Voicer.new(notes).high_range
+composition = Chromatic::Composer.new.composition
+#notes = composition[:staves].first[:notes]
+#staves_notes = {}
 
-    score = [
-      staff_1: {
-        notes: [0, [2, 4, 5], 8, 11, 4],
-      },
-      staff_2: {
-        notes: [2, 8, 11, 4],
-      },
-    ]
-
-    LilyConverter.new(notes: notes).convert
-    #"<c' d' e' f'> <c' e' a>"
-  end
+staves_notes = composition[:staves].map do |staff|
+  ly_notes_to_write = Chromatic::LilyConverter.new(notes: staff[:notes]).convert
 end
 
-ly_notes_to_write = NoteBuilder.new.ly_notes
+
+#ly_notes_to_write = Chromatic::LilyConverter.new(notes: notes).convert
+template_stuffs_to_stuff = TemplateStuffer.stuff(staves_notes: staves_notes)
 
 template = IO.read(TEMPLATE_FILE)
+ly_notes_to_write = "c' d''"
 template.sub!('%%%INSERT_NOTE_CONTENTS_HERE%%%', ly_notes_to_write)
+template.sub!('%%%INSERT_ADDITIONAL_STAVES_HERE%%%', template_stuffs_to_stuff)
 
 File.open(OUTPUT_FILE, 'w') do |f|
   f.write(template)
