0d648473cdbb1f8ff4ba6431a5d69fdd9b4db37b
Mark Oates <markoates0@gmail.com>
2019-04-19 19:54:58 -0400

    Expand composition

--- maker/app/composers/second_suite.rb
+++ maker/app/composers/second_suite.rb
@@ -1,6 +1,48 @@
 require_relative '../composer_base'
 
+class Note
+  attr_reader :pitches, :duration
+
+  def initialize(pitches:, duration:)
+    @pitches = pitches
+    @duration = duration
+  end
+
+  def call
+    7
+  end
+end
+
 class SecondSuite < ComposerBase
+  DIATONIC = [0, 2, 4, 5, 7, 9, 11]
+
+  def next_diatonic(chromatic:, interval: 1)
+    index = DIATONIC.find_index(chromatic)
+    DIATONIC[index + interval]
+  end
+
+  def ascend_sequence(cell:)
+    3.times.map do |i|
+      [next_diatonic(chromatic: 0, interval: i),
+       next_diatonic(chromatic: 4, interval: i),
+       next_diatonic(chromatic: 2, interval: i),
+       'r']
+    end.flatten
+  end
+
+  def harmonize_in_3rds(cell:)
+    cell.each_with_index.map do |value, index|
+      if value == 'r'
+        'r'
+      else
+        [
+          value,
+          next_diatonic(chromatic: value, interval: 2),
+        ]
+      end
+    end
+  end
+
   def composition
     projections = Chromatic::ChordNotes.notes_for('circle_of_5ths')
     projections.concat(Chromatic::ChordNotes.notes_for('ascending_bass'))
@@ -10,6 +52,8 @@ class SecondSuite < ComposerBase
     melody.pop
     melody << 12  # make sure it ends on tonic
 
+    cell = [0, 4, 2, 'r']
+
     result = {
       staves: [
         {
@@ -18,12 +62,14 @@ class SecondSuite < ComposerBase
         },
         {
           instrument: { name: { full: 'Oboe', abbreviated: 'Ob.', } },
-          notes: melody.each_with_index.map { |projection, index| index.even? ? 'r' : projection },
-          #notes: melody,
+          notes: melody,
+          #notes: melody.each_with_index.map { |projection, index| index.even? ? 'r' : projection },
         },
         {
           instrument: { name: { full: 'Clarinet in Bb', abbreviated: 'Cl.', } },
-          notes: projections.map { |projection| Chromatic::Voicer.new(projection).open },
+          #notes: [0, 4, 2, 'r', Note(duration: 2, pitches: [0, 4, 7])] #projections.map { |projection| Chromatic::Voicer.new(projection).open },
+          notes: harmonize_in_3rds(cell: ascend_sequence(cell: cell)),
+          #notes: projections.map { |projection| Chromatic::Voicer.new(projection).open },
         },
         {
           instrument: { name: { full: 'Horn in F', abbreviated: 'F Hn.', } },
@@ -32,6 +78,11 @@ class SecondSuite < ComposerBase
         {
           instrument: { name: { full: 'Bassoon', abbreviated: 'Bsn.', }, clef: 'bass', },
           notes: projections.map { |projection| projection.first - 12 },
+        },
+        {
+          instrument: { name: { full: 'Piano (R.H.)', abbreviated: 'Pf-R.', }, },
+          notes: projections.map { |projection| Chromatic::Voicer.new(projection).open },
+          #notes: projections.map { |projection| projection.first - 12 },
         },
       ]
     }
