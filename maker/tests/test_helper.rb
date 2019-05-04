23e0d8a22f30a510bcba2381b12e67d61a0531fb
Mark Oates <markoates0@gmail.com>
2019-05-04 14:16:27 -0400

    Move minitest reporter options into REPORTER= env var

--- maker/tests/test_helper.rb
+++ maker/tests/test_helper.rb
@@ -2,21 +2,19 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 
 
-## will use the default reporter for the minitest-reporters gem, which is a progress bar
-#require "minitest/reporters"
-#Minitest::Reporters.use!
+case ENV['REPORTER']
+when 'JsonReporter'
+  require 'minitest/reporters/json_reporter'
+  Minitest::Reporters.use! [ Minitest::Reporters::JsonReporter.new ]
+when 'ProgressReporter'
+  require "minitest/reporters"
+  Minitest::Reporters.use!
+when 'HtmlReporter'
+  require 'minitest/reporters'
+  Minitest::Reporters.use! [ Minitest::Reporters::HtmlReporter.new ]
+else
+  require 'minitest/reporters'
+  Minitest::Reporters.use! [ Minitest::Reporters::DefaultReporter.new(:color => true) ]
+end
 
 
-## will output test results a JSON string
-#require 'minitest/reporters/json_reporter'
-#Minitest::Reporters.use! [ Minitest::Reporters::JsonReporter.new ]
-
-
-## will output an html report (looks nice) to test/html_reports/index.html
-#require 'minitest/reporters'
-#Minitest::Reporters.use! [ Minitest::Reporters::HtmlReporter.new ]
-
-
-# will output the usual output, but colored green/red for success/failure
-require 'minitest/reporters'
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
