ff3b8f62f254d155ff64c2f12c9391916ea1682c
Mark Oates <markoates0@gmail.com>
2019-06-01 22:39:30 -0400

    Add more features to ChromaticVector

--- maker/lib/vector.rb
+++ maker/lib/vector.rb
@@ -1,12 +1,32 @@
 class ChromaticVector
-  attr_reader :start_chord_quality, :direction, :direction_quality
+  attr_reader :direction
 
-  def initialize(start_chord_quality:, direction_root:, direction_quality:)
-    @start_chord_quality = start_chord_quality
+  def initialize(direction:)
     @direction = direction
-    @direction_quality = direction_quality
   end
 
   def render
+    case direction
+    when 'i'
+      [ 9, 12, 16 ]
+    end
+  end
+
+  private
+
+  def minor_from(root:)
+    [ root, root + 3, root + 7 ]
+  end
+
+  def major_from(root:)
+    [ root, root + 4, root + 7 ]
+  end
+
+  def augmented_from(root:)
+    [ root, root + 4, root + 8 ]
+  end
+
+  def diminished_from(root:)
+    [ root, root + 3, root + 6 ]
   end
 end
--- maker/tests/vector_test.rb
+++ maker/tests/vector_test.rb
@@ -2,15 +2,25 @@ require_relative '../lib/vector'
 require_relative 'test_helper'
 
 class ChromaticVectorTest < Minitest::Test
-  def chromatic_vector
-    @chromatic_vector ||= ChromaticVector.new(
-      start_chord_quality: nil,
-      direction_root:      nil,
-      direction_quality:   nil,
-    )
+  def chromatic_vector(direction:)
+    @chromatic_vector ||= ChromaticVector.new(direction: direction)
   end
 
   def test_chromatic_vector_doenst_blow_up
-    chromatic_vector.render
+    chromatic_vector(direction: 'i').render
+  end
+
+  def test_chromatic_vector_renders_the_expected_vector
+    expected = {
+      result: [ 9, 0+12, 4+12 ],
+    }
+    chromatic_vector(direction: 'vi').render
+  end
+
+  def test_chromatic_vector_renders_the_expected_vector
+    expected = {
+      result: [ 9, 0+12, 4+12 ],
+    }
+    chromatic_vector(direction: 'vi').render
   end
 end
