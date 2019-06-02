77351b20a1d5a099cfbbe0153cc078e2a191e0c9
Mark Oates <markoates0@gmail.com>
2019-06-02 01:44:33 -0400

    Enable main diatonic chord types; Introduce quality

--- maker/lib/chromatic/chord_builder.rb
+++ maker/lib/chromatic/chord_builder.rb
@@ -7,13 +7,36 @@ class ChordBuilder
   end
 
   def build
-    get_base_string
+    base_string = get_base_string
+
+    case quality
+    when 'minor'
+      base_string
+    when 'major'
+      base_string.upcase
+    else
+      raise UnknownChordQuality, "chord quality not recognized: \"#{qualith}\""
+    end
   end
+
+  private
 
   def get_base_string
     case root
     when 0
       'i'
+    when 2
+      'ii'
+    when 4
+      'iii'
+    when 5
+      'iv'
+    when 7
+      'v'
+    when 9
+      'vi'
+    when 11
+      'vii'
     else
       raise StandardError
     end
--- maker/tests/chromatic/chord_builder_test.rb
+++ maker/tests/chromatic/chord_builder_test.rb
@@ -3,10 +3,19 @@ require_relative '../test_helper'
 
 module Chromatic
   class ChordBuilderTest < Minitest::Test
-    def test_plots_notes_in_a_chord
+    def test_plots_notes_in_a_minor_chord
       builder = ChordBuilder.new(root: 0, quality: 'minor')
 
       expcected = 'i'
+      actual = builder.build
+
+      assert_equal expcected, actual
+    end
+
+    def test_plots_notes_in_a_major_chord
+      builder = ChordBuilder.new(root: 0, quality: 'major')
+
+      expcected = 'I'
       actual = builder.build
 
       assert_equal expcected, actual
