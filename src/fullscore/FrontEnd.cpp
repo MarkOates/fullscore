5d3e0ab56038a543bee028f95fc99f26e360ba5e
Mark Oates <markoates0@gmail.com>
2019-09-01 05:14:53 -0400

    Expand "FrontEnd" experiment with rotation

--- src/fullscore/FrontEnd.cpp
+++ src/fullscore/FrontEnd.cpp
@@ -22,7 +22,11 @@ FrontEnd::~FrontEnd()
 
 void FrontEnd::on_draw()
 {
+static placement2d place;
+place.rotation = place.rotation += 0.01;
+place.start_transform();
 al_draw_filled_rectangle(300, 400, 900, 1200, al_color_name("green"));
+place.restore_transform();
 return;
 
 }
