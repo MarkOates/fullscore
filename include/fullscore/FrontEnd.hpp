4e24a3ff987a6c63f72ddc7c6284e0647d62c1bf
Mark Oates <markoates0@gmail.com>
2019-09-01 02:51:53 -0400

    Add "fullscore/FrontEnd", derives from UIScreen

---
+++ include/fullscore/FrontEnd.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include <allegro_flare/gui/gui_screen.h>
+
+
+namespace fullscore
+{
+   class FrontEnd : private UIScreen
+   {
+   private:
+
+   public:
+      FrontEnd();
+      ~FrontEnd();
+
+
+   };
+}
+
+
+
---
+++ src/fullscore/FrontEnd.cpp
@@ -0,0 +1,24 @@
+
+
+#include <fullscore/FrontEnd.hpp>
+
+
+
+namespace fullscore
+{
+
+
+FrontEnd::FrontEnd()
+   : UIScreen(display)
+{
+}
+
+
+FrontEnd::~FrontEnd()
+{
+}
+
+
+} // namespace fullscore
+
+
