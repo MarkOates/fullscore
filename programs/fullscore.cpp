74e909e288315c0af397550f8caddef08cc70e40
Mark Oates <markoates0@gmail.com>
2019-09-01 03:13:41 -0400

    Fix comma that unfortunately does not compile

--- programs/fullscore.cpp
+++ programs/fullscore.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
    Config config = Framework::get_config();
    Display *d = Framework::create_display(
          config.get_or_default_int("GLOBAL_SETTINGS", "resolution_x", 800),
-         config.get_or_default_int("GLOBAL_SETTINGS", "resolution_y", 600),
+         config.get_or_default_int("GLOBAL_SETTINGS", "resolution_y", 600)
       );
    AppController app_controller(d, config);
    Framework::run_loop();
