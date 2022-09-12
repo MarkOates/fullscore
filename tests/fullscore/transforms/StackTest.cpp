3801c1aba720937e012b34fdf33cd245bce313c2
Mark Oates <markoates0@gmail.com>
2022-09-12 17:05:57 -0400

    Restore now working test

--- tests/fullscore/transforms/StackTest.cpp
+++ tests/fullscore/transforms/StackTest.cpp
@@ -37,13 +37,10 @@ TEST(TransformStackTest, executes_the_sequence_of_transforms)
    for (unsigned i=0; i<3; i++)
       transform_stack.add_transform(&insert_note_transform);
 
+   std::vector<Note> expected_notes = { Note(), Note(), Note() };
+   std::vector<Note> returned_notes = transform_stack.transform(source_notes);
 
-   // TODO: this test is segfaulting
-
-   //std::vector<Note> expected_notes = { Note(), Note(), Note() };
-   //std::vector<Note> returned_notes = transform_stack.transform(source_notes);
-
-   //EXPECT_EQ(expected_notes, returned_notes);
+   EXPECT_EQ(expected_notes, returned_notes);
 }
 
 
